2d52089d5d75c4bd8e22504deb4ad6999c781969
mirinta <mirinta@163.com>
2023-04-16 03:00:16 +0800

    minor changes

--- array/27_remove_element.cpp
+++ array/27_remove_element.cpp
@@ -1,38 +1,31 @@
 #include <vector>
 
-/// Given an integer array "nums" and an integer "val", remove all occurrences of "val" in "nums"
-/// in-place. The order of the elements may be changed. Then return the number of elements in "nums"
-/// which are not equal to "val".
-///
-/// Consider the number of elements in "nums" which are not equal to "val" be "k", to get accepted,
-/// you need to do the following things:
-///
-/// - Change the array "nums" such that the first "k" elements of "nums" contain the elements which
-/// are not equal to "val". The remaining elements of nums are not important as well as the size of
-/// nums.
-///
-/// - Return "k".
+/**
+ * Given an integer array "nums" and an integer "val", remove all occurrences of "val" in "nums"
+ * in-place. The order of the elements may be changed. Then return the number of elements in "nums"
+ * which are not equal to "val".
+ *
+ * Consider the number of elements in "nums" which are not equal to "val" be "k", to get accepted,
+ * you need to do the following things:
+ *
+ * - Change the array "nums" such that the first "k" elements of "nums" contain the elements which
+ * are not equal to "val". The remaining elements of nums are not important as well as the size of
+ * nums.
+ *
+ * - Return "k".
+ */
 
 class Solution
 {
 public:
-    void moveZeroes(std::vector<int>& nums)
+    int removeElement(std::vector<int>& nums, int val)
     {
-        const auto size = nums.size();
-        if (size <= 1)
-            return;
-
-        // similar to leet code 27:
-        // move all non-zero elements to the front of the array
-        size_t nonZeroCount = 0;
-        for (size_t i = 0; i < size; ++i) {
-            if (nums[i] != 0) {
-                nums[nonZeroCount++] = nums[i];
+        size_t keep = 0;
+        for (size_t search = 0; search < nums.size(); ++search) {
+            if (nums[search] != val) {
+                nums[keep++] = nums[search];
             }
         }
-        // set all remaining elements to 0
-        for (; nonZeroCount < size; ++nonZeroCount) {
-            nums[nonZeroCount] = 0;
-        }
+        return keep; // warning: narrowing conversion from size_t to int
     }
 };
--- array/704_binary_search.cpp
+++ array/704_binary_search.cpp
@@ -1,15 +1,17 @@
 #include <vector>
 
-/// Given an array of integers "nums" which is sorted in ascending order, and an integer "target",
-/// write a function to search target in "nums". If "target" exists, then return its index.
-/// Otherwise, return -1.
-///
-/// You must write an algorithm with O(log n) runtime complexity.
+/**
+ * Given an array of integers "nums" which is sorted in ascending order, and an integer "target",
+ * write a function to search target in "nums". If "target" exists, then return its index.
+ * Otherwise, return -1.
+ *
+ * You must write an algorithm with O(log n) runtime complexity.
+ */
 
 class Solution
 {
 public:
-    /// time complexity: O(n), space complexity: O(1)
+    // time complexity: O(n), space complexity: O(1)
     int search(const std::vector<int>& nums, int target)
     {
         if (nums.empty())
