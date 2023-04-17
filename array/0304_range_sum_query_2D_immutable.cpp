a49f6dfc59fd8678ca974e3e1ea4b5096d506bc9
mirinta <mirinta@163.com>
2023-04-17 20:58:27 +0800

    leet code 304

--- array/0304_range_sum_query_2D_immutable.cpp
+++ array/0304_range_sum_query_2D_immutable.cpp
@@ -14,20 +14,39 @@
  * "matrix" inside the rectangle defined by its upper left corner "(row1, col1)" and lower right
  * corner "(row2, col2)".
  *
- * You must design an algorithm where "sumRegion" works on O(1) time complexity.
+ * ! You must design an algorithm where "sumRegion" works on O(1) time complexity.
  */
 
 class NumMatrix
 {
 public:
     NumMatrix(std::vector<std::vector<int>>& matrix)
-        : _preSums(matrix.size(), std::vector<int>(matrix[0].size(), 0))
+        : _preSums(matrix.size() + 1, std::vector<int>(matrix[0].size() + 1, 0))
     {
+        for (size_t row = 0; row < matrix.size(); ++row) {
+            for (size_t col = 0; col < matrix[row].size(); ++col) {
+                _preSums[row + 1][col + 1] = _preSums[row][col + 1] + _preSums[row + 1][col] +
+                                             matrix[row][col] - _preSums[row][col];
+            }
+        }
     }
 
-    int sumRegion(int row1, int col1, int row2, int col2) {}
+    int sumRegion(int row1, int col1, int row2, int col2)
+    {
+        return _preSums[row1][col1] + _preSums[row2 + 1][col2 + 1] - _preSums[row1][col2 + 1] -
+               _preSums[row2 + 1][col1];
+    }
 
 private:
+    // suppose the origin is a 3x3 matrix:
+    // x00      x01     x02
+    // x10      x11     x12
+    // x20      x21     x22
+    // then our _preSums is a 4x4 matrix:
+    // 0        0               0                           0
+    // 0        x00             x00+x01                     x00+x01+x02
+    // 0        x00+x10         x00+x01+x10+x11             x00+x01+x02+x10+x11+x12
+    // 0        x00+x10+x20     x00+x01+x10+x11+x20+x21     x00+x01+x02+x10+x11+x12+x20+x21+x22
     std::vector<std::vector<int>> _preSums;
 };
 
