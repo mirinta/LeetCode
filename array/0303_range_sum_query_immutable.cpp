083c3932b206c6cd3f74246068487af905345416
mirinta <mirinta@163.com>
2023-04-17 21:36:52 +0800

    minor changes

--- array/0303_range_sum_query_immutable.cpp
+++ array/0303_range_sum_query_immutable.cpp
@@ -13,24 +13,27 @@
  * - "int sumRange(int left, int right)" returns the sum of the elements of "nums" between indices
  * "left" and "right" inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
  *
- * !At most 10^4 calls will be made to "sumRange".
+ * ! At most 10^4 calls will be made to "sumRange".
  */
 
 class NumArray
 {
-public:
-    NumArray(std::vector<int>& nums) : _preSums(nums.size() + 1, 0)
+    // [0, sumRange[0,0], sumRange[0,1], ..., sumRange[0, N-1]]
+    const std::vector<int> _preSums;
+
+    static std::vector<int> createPreSums(const std::vector<int>& nums)
     {
+        std::vector<int> result(nums.size() + 1, 0);
         for (size_t i = 0; i < nums.size(); ++i) {
-            _preSums[i + 1] = _preSums[i] + nums[i];
+            result[i + 1] = result[i] + nums[i];
         }
+        return result;
     }
 
-    int sumRange(int left, int right) { return _preSums[right + 1] - _preSums[left]; }
+public:
+    NumArray(std::vector<int>& nums) : _preSums(createPreSums(nums)) {}
 
-private:
-    // [0, sumRange[0,0], sumRange[0,1], ..., sumRange[0, N-1]]
-    std::vector<int> _preSums;
+    int sumRange(int left, int right) { return _preSums[right + 1] - _preSums[left]; }
 };
 
 /**
--- array/0304_range_sum_query_2D_immutable.cpp
+++ array/0304_range_sum_query_2D_immutable.cpp
@@ -19,25 +19,6 @@
 
 class NumMatrix
 {
-public:
-    NumMatrix(std::vector<std::vector<int>>& matrix)
-        : _preSums(matrix.size() + 1, std::vector<int>(matrix[0].size() + 1, 0))
-    {
-        for (size_t row = 0; row < matrix.size(); ++row) {
-            for (size_t col = 0; col < matrix[row].size(); ++col) {
-                _preSums[row + 1][col + 1] = _preSums[row][col + 1] + _preSums[row + 1][col] +
-                                             matrix[row][col] - _preSums[row][col];
-            }
-        }
-    }
-
-    int sumRegion(int row1, int col1, int row2, int col2)
-    {
-        return _preSums[row1][col1] + _preSums[row2 + 1][col2 + 1] - _preSums[row1][col2 + 1] -
-               _preSums[row2 + 1][col1];
-    }
-
-private:
     // suppose the origin is a 3x3 matrix:
     // x00      x01     x02
     // x10      x11     x12
@@ -47,7 +28,29 @@ private:
     // 0        x00             x00+x01                     x00+x01+x02
     // 0        x00+x10         x00+x01+x10+x11             x00+x01+x02+x10+x11+x12
     // 0        x00+x10+x20     x00+x01+x10+x11+x20+x21     x00+x01+x02+x10+x11+x12+x20+x21+x22
-    std::vector<std::vector<int>> _preSums;
+    using Matrix = std::vector<std::vector<int>>;
+    const Matrix _preSums;
+
+    static Matrix createPreSums(const Matrix& matrix)
+    {
+        Matrix result(matrix.size() + 1, std::vector<int>(matrix[0].size() + 1, 0));
+        for (size_t row = 0; row < matrix.size(); ++row) {
+            for (size_t col = 0; col < matrix[row].size(); ++col) {
+                result[row + 1][col + 1] = result[row][col + 1] + result[row + 1][col] +
+                                           matrix[row][col] - result[row][col];
+            }
+        }
+        return result;
+    }
+
+public:
+    NumMatrix(Matrix& matrix) : _preSums(createPreSums(matrix)) {}
+
+    int sumRegion(int row1, int col1, int row2, int col2)
+    {
+        return _preSums[row1][col1] + _preSums[row2 + 1][col2 + 1] - _preSums[row1][col2 + 1] -
+               _preSums[row2 + 1][col1];
+    }
 };
 
 /**
