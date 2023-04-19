b4a9bedcb5e2452c2f18233047e9cad283c4bf16
mirinta <mirinta@163.com>
2023-04-19 20:37:58 +0800

    leet code 76

--- string/0076_minimum_window_substring.cpp
+++ string/0076_minimum_window_substring.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <unordered_map>
 
 /**
  * Given two strings "s" and "t" of lengths "m" and "n" respectively, return the minimum window
@@ -21,5 +22,56 @@
 class Solution
 {
 public:
-    std::string minWindow(const std::string& s, const std::string& t) {}
+    // step 1: hold "left" and move "right", expand the window, try to find a "feasible solution"
+    //     |------------------|
+    // s = | e  b  b  a  n  c | f, t = "abc"
+    //     |------------------|
+    //    left               right
+    //
+    // step 2: hold "right" and move "left", shrink the window, try to find a "better solution"
+    //             |---------|
+    // s = e  b  b | a  n  c | f, t = "abc"
+    //             |---------|
+    //            left      right
+    //
+    // step 3: repeat 2 and 3 until "right" touches the end
+    std::string minWindow(const std::string& s, const std::string& t)
+    {
+        if (s.empty() || t.empty() || s.size() < t.size())
+            return {};
+
+        // record the "optimal" solution
+        int minStart = 0;
+        int minLength = INT_MAX;
+        // to check a substring is valid
+        std::unordered_map<char, int> freq;
+        for (const auto& c : t) {
+            freq[c]++;
+        }
+        int counter = freq.size(); // nubmer of unique characters of t
+        // sliding window, characters in range [left, right)
+        int left = 0;
+        int right = 0;
+        while (right < s.size()) {
+            // expand the window to find a feasible solution
+            const auto& charRight = s[right++];
+            if (--freq[charRight] == 0) {
+                counter--;
+            }
+            // counter = 0 means a feasible solution is found
+            while (counter == 0) {
+                // update the optimal solution
+                if (right - left < minLength) {
+                    minStart = left;
+                    minLength = right - left;
+                }
+                // shrink the window to find a better solution
+                const auto& charLeft = s[left++];
+                if (++freq[charLeft] > 0) {
+                    counter++;
+                }
+            }
+        }
+        return minLength == INT_MAX ? "" : s.substr(minStart, minLength);
+    }
 };
