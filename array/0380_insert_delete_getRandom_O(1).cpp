b71beb56536eae147ee8ea89c7ea553adfe009ea
mirinta <mirinta@163.com>
2023-04-23 02:30:00 +0800

    not done

---
+++ array/0380_insert_delete_getRandom_O(1).cpp
@@ -0,0 +1,38 @@
+/**
+ * Implement the "RandomizedSet" class:
+ *
+ * - "RandomizedSet()" initializes the "RandomizedSet" object.
+ *
+ * - "bool insert(int val)" inserts an item "val" into the set if not present. Returns "true" if the
+ * item was not present, "false" otherwise.
+ *
+ * - "bool remove(int val)" removes an item "val" from the set if present. Returns "true" if the
+ * item was present, "false" otherwise.
+ *
+ * - "int getRandom()" returns a random element from the current set of elements (it's guaranteed
+ * that at least one element exists when this method is called). Each element must have the same
+ * probability of being returned.
+ *
+ * ! You must implement the functions of the class such that each function works in average O(1)
+ * ! time complexity.
+ */
+
+class RandomizedSet
+{
+public:
+    RandomizedSet() {}
+
+    bool insert(int val) {}
+
+    bool remove(int val) {}
+
+    int getRandom() {}
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
