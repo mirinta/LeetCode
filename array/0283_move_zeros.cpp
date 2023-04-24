e3fe6686803bb476fbf4c43f7bee6a5f9f70cf42
mirinta <mirinta@163.com>
2023-04-25 03:03:14 +0800

    not done

--- array/0283_move_zeros.cpp
+++ array/0283_move_zeros.cpp
@@ -12,21 +12,32 @@ class Solution
 public:
     void moveZeroes(std::vector<int>& nums)
     {
-        const auto size = nums.size();
-        if (size <= 1)
-            return;
+        // version 1:
+        // const auto size = nums.size();
+        // if (size <= 1)
+        //     return;
 
         // similar to leet code 27:
         // move all non-zero elements to the front of the array
-        size_t nonZeroCount = 0;
-        for (size_t i = 0; i < size; ++i) {
-            if (nums[i] != 0) {
-                nums[nonZeroCount++] = nums[i];
-            }
-        }
+        // size_t nonZeroCount = 0;
+        // for (size_t i = 0; i < size; ++i) {
+        //     if (nums[i] != 0) {
+        //         nums[nonZeroCount++] = nums[i];
+        //     }
+        // }
         // set all remaining elements to 0
-        for (; nonZeroCount < size; ++nonZeroCount) {
-            nums[nonZeroCount] = 0;
+        // for (; nonZeroCount < size; ++nonZeroCount) {
+        //     nums[nonZeroCount] = 0;
+        // }
+        // version 2:
+        if (nums.size() <= 1)
+            return;
+
+        size_t nonZero = 0;
+        for (size_t i = 0; i < nums.size(); ++i) {
+            if (nums[i] != 0) {
+                std::swap(nums[i], nums[nonZero++]);
+            }
         }
     }
 };
---
+++ string/0844_backspace_string_compare.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+/**
+ * Given two strings "s" and "t", return "true" if they are equal when both are typed into empty
+ * text editors. "#" means a backspace character.
+ *
+ * Note that after backspacing an empty text, the text will continue empty.
+ */
+
+class Solution
+{
+public:
+    bool backspaceCompare(const std::string& s, const std::string& t)
+    {
+        if (s.empty() && t.empty())
+            return true;
+    }
+};
