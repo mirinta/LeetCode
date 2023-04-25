426b1a9e279f6c461340afabe3236f00488645c0
Mirinta <mirinta@163.com>
2023-04-25 15:08:51 +0800

    Update 0844_backspace_string_compare.cpp

--- string/0844_backspace_string_compare.cpp
+++ string/0844_backspace_string_compare.cpp
@@ -1,3 +1,4 @@
+#include <stack>
 #include <string>
 
 /**
@@ -12,7 +13,53 @@ class Solution
 public:
     bool backspaceCompare(const std::string& s, const std::string& t)
     {
-        if (s.empty() && t.empty())
-            return true;
+        // version 1: two stacks:
+        std::stack<char> stackS;
+        process(stackS, s);
+        std::stack<char> stackT;
+        process(stackT, t);
+        if (stackS.size() != stackT.size())
+            return false;
+
+        for (; !stackS.empty() && !stackT.empty(); stackS.pop(), stackT.pop()) {
+            if (stackS.top() != stackT.top())
+                return false;
+        }
+        return true;
+        // version 2: two pointers
+        // const char backspace = '#';
+        // int idxS = s.size() - 1;
+        // int countS = 0;
+        // int idxT = t.size() - 1;
+        // int countT = 0;
+        // while (true) {
+        //     while (idxS >= 0 && (s[idxS] == backspace || countS > 0)) {
+        //         s[idxS--] == backspace ? countS++ : countS--;
+        //     }
+        //     while (idxT >= 0 && (t[idxT] == backspace || countT > 0)) {
+        //         t[idxT--] == backspace ? countT++ : countT--;
+        //     }
+        //     if (idxS < 0 || idxT < 0)
+        //         break;
+
+        //     if (s[idxS--] != t[idxT--])
+        //         return false;
+        // }
+        // return idxS == idxT;
+    }
+
+private:
+    void process(std::stack<char>& stack, const std::string& s)
+    {
+        const char backspace = '#';
+        for (const auto& c : s) {
+            if (c == backspace) {
+                if (!stack.empty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(c);
+            }
+        }
     }
 };
