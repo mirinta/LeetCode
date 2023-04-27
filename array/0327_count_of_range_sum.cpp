a7a8013b15ced54ab05e2746b43fcc980651ca27
mirinta <mirinta@163.com>
2023-04-28 02:14:57 +0800

    not done

---
+++ array/0327_count_of_range_sum.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+/**
+ * Given an integer array "nums" and two integers "lower" and "upper", return the number of range
+ * sums that lie in "[lower, upper]" inclusive.
+ *
+ * Range sum "S(i, j)" is defined as the sum of the elements in "nums" between indicies "i" and "j"
+ * inclusive, where "i <= j".
+ */
+
+class Solution
+{
+public:
+    int countRangeSum(std::vector<int>& nums, int lower, int upper) {}
+};
