0d5b496fc39af7becba66360a8b53d0852321a7a
mirinta <mirinta@163.com>
2023-04-28 14:58:49 +0800

    Add files via upload

---
+++ binary_tree/0538_convert_BST_to_greater_tree.cpp
@@ -0,0 +1,58 @@
+/**
+ Definition for a binary tree node.
+ */
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Given the "root" of a BST, convert it to a Greater Tree such that every key of the original BST
+ * is changed to the original key plus all the sum of all keys greater than the original key in BST.
+ *
+ * As a reminder, a BST is a tree that satisfies these constraints:
+ *
+ * - The left subtree of a node contains only nodes with keys less than the node's key.
+ *
+ * - The right subtree of a node contains only nodes with keys greater than the node's key.
+ *
+ * - Both the left and right subtrees must also be BSTs.
+ *
+ * ! All the values in the tree are unique.
+ *
+ * ! "root" is guaranteed to be a valid BST.
+ *
+ * ! This question is the same as 1038.
+ */
+
+class Solution
+{
+public:
+    TreeNode* convertBST(TreeNode* root)
+    {
+        if (!root)
+            return nullptr;
+
+        traverse(root);
+        return root;
+    }
+
+private:
+    int sum = 0;
+
+    void traverse(TreeNode* node)
+    {
+        if (!node)
+            return;
+
+        traverse(node->right); // traverse in descending order
+        sum += node->val;
+        node->val = sum;
+        traverse(node->left);
+    }
+};
