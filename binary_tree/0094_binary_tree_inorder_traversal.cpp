03db1861e6d7b964410b6ec2512ad424d780682a
mirinta <mirinta@163.com>
2023-05-02 02:15:22 +0800

    leet coe 94, 145, 239, and 347

---
+++ binary_tree/0094_binary_tree_inorder_traversal.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+
+/**
+ Definition for a binary tree node.
+ */
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Given the "root" of a binary tree, return the inorder traversal of its nodes' values.
+ */
+
+class Solution
+{
+public:
+    std::vector<int> inorderTraversal(TreeNode* root)
+    {
+        if (!root)
+            return {};
+
+        std::vector<int> result;
+        traverse(result, root);
+        return result;
+    }
+
+private:
+    void traverse(std::vector<int>& result, TreeNode* node)
+    {
+        if (!node)
+            return;
+
+        traverse(result, node->left);
+        result.push_back(node->val);
+        traverse(result, node->right);
+    }
+};
