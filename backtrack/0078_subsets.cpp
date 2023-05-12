0b04126732ff9afb642c099325cd7cd5d6a9d0d1
mirinta <mirinta@163.com>
2023-05-12 11:22:57 +0800

    Update 0078_subsets.cpp

--- backtrack/0078_subsets.cpp
+++ backtrack/0078_subsets.cpp
@@ -17,3 +17,30 @@
  * Input: nums = [0]
  * Output: nums = [[], [0]]
  */
+
+class Solution
+{
+public:
+    std::vector<std::vector<int>> subsets(std::vector<int>& nums)
+    {
+        if (nums.empty())
+            return {};
+
+        std::vector<int> subset;
+        backtrack(nums, 0, subset);
+        return result;
+    }
+
+private:
+    std::vector<std::vector<int>> result;
+
+    void backtrack(const std::vector<int>& nums, int start, std::vector<int>& subset)
+    {
+        result.push_back(subset);
+        for (int i = start; i < nums.size(); ++i) {
+            subset.push_back(nums[i]);
+            backtrack(nums, i + 1, subset);
+            subset.pop_back();
+        }
+    }
+};
