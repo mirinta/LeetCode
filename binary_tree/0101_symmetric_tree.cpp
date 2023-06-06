9b0ae9a877a1d0cade333680cca6448ba1baebfe
mirinta <mirinta@163.com>
2023-06-06 11:54:45 +0800

    Update 0101_symmetric_tree.cpp

--- binary_tree/0101_symmetric_tree.cpp
+++ binary_tree/0101_symmetric_tree.cpp
@@ -21,11 +21,46 @@ struct TreeNode
 
 class Solution
 {
+public:
+    bool isSymmetric(TreeNode* root)
+    {
+        if (!root)
+            return false;
+
+        return approach3(root->left, root->right);
+    }
+
 private:
+    bool approach3(TreeNode* root1, TreeNode* root2)
+    {
+        if (!root1 && !root2)
+            return true;
+
+        if (!root1 || !root2)
+            return false;
+
+        return (root1->val == root2->val) && approach3(root1->left, root2->right) &&
+               approach3(root1->right, root2->left);
+    }
+
+    bool approach2(TreeNode* root)
+    {
+        if (!root)
+            return false;
+
+        return compare(root->left, root->right);
+    }
+
     bool compare(TreeNode* node1, TreeNode* node2)
     {
-        if (!node1 || !node2)
-            return node1 == node2;
+        if (!node1 && node2)
+            return false;
+
+        if (node1 && !node2)
+            return false;
+
+        if (!node1 && !node2)
+            return true;
 
         if (node1->val != node2->val)
             return false;
@@ -33,17 +68,7 @@ private:
         return compare(node1->left, node2->right) && compare(node1->right, node2->left);
     }
 
-public:
-    // approach 2: recursion
-    // bool isSymmetric(TreeNode* root)
-    // {
-    //     if (!root)
-    //         return false;
-
-    //     return compare(root->left, root->right);
-    // }
-    // approach 1: level order traversal
-    bool isSymmetric(TreeNode* root)
+    bool approach1(TreeNode* root)
     {
         if (!root)
             return false;
@@ -61,8 +86,8 @@ public:
                 }
             }
             for (size_t i = 0, j = nodes.size() - 1; i < j; ++i, --j) {
-                auto* left = nodes[i];
-                auto* right = nodes[j];
+                const auto* const left = nodes[i];
+                const auto* const right = nodes[j];
                 if (left && !right)
                     return false;
 
