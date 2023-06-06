3632a7ca1737a46e45592729e18fc58c6659316e
mirinta <mirinta@163.com>
2023-06-06 17:10:36 +0800

    剑指Offer.005

---
+++ 剑指Offer名企面试官精讲典型编程题_第2版/005_替换空格.cpp
@@ -0,0 +1,63 @@
+#include <string>
+
+/**
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ */
+
+class Solution
+{
+public:
+    std::string replaceSpace(std::string s)
+    {
+        approach2(s);
+        return s;
+    }
+
+private:
+    // approach 1: straightforward
+    std::string approach1(const std::string& s)
+    {
+        if (s.empty())
+            return {};
+
+        std::string result;
+        for (const auto& c : s) {
+            if (c == ' ') {
+                result.append("%20");
+            } else {
+                result.push_back(c);
+            }
+        }
+        return result;
+    }
+
+    // approach 2: in-place modification
+    void approach2(std::string& s)
+    {
+        if (s.empty())
+            return;
+
+        int numOfSpaces = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == ' ') {
+                numOfSpaces++;
+            }
+        }
+        if (numOfSpaces == 0)
+            return;
+
+        int originEnd = s.size() - 1;
+        s.resize(s.size() + 2 * numOfSpaces);
+        int newEnd = s.size() - 1;
+        while (originEnd >= 0 && newEnd > originEnd) {
+            if (s[originEnd] != ' ') {
+                s[newEnd--] = s[originEnd--];
+            } else {
+                s[newEnd--] = '0';
+                s[newEnd--] = '2';
+                s[newEnd--] = '%';
+                originEnd--;
+            }
+        }
+    }
+};
