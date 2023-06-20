24c98e66cc0d9a6f61af9d9d14344d335645e7e6
mirinta <mirinta@163.com>
2023-06-20 16:23:42 +0800

    Update 0069_sqrt(x).cpp

--- binary_search/0069_sqrt(x).cpp
+++ binary_search/0069_sqrt(x).cpp
@@ -1,22 +1,22 @@
-
 /**
  * Given a non-negative integer "x", return the square root of "x" rounded down to the nearest
  * integer. The returned integer should be non-negative as well.
  *
  * You must not use any built-in exponent function or operator.
- * 
+ *
  * ! 1 <= num <= 2^31 - 1
  */
 
-class Solution 
+class Solution
 {
 public:
-    bool isPerfectSquare(int num) {
+    bool isPerfectSquare(int num)
+    {
         if (num < 2)
             return true; // the constraint tells us 1 <= num <= INT_MAX
 
         long long left = 2;
-        long long right = num / 2; 
+        long long right = num / 2;
         while (left <= right) {
             long long mid = left + (right - left) / 2;
             const long long product = mid * mid;
