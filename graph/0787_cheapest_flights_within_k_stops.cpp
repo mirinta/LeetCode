3e836807d4cb13dcbe793c2c005c50369236cbaf
mirinta <mirinta@163.com>
2023-07-06 16:33:14 +0800

    Update 0787_cheapest_flights_within_k_stops.cpp

--- graph/0787_cheapest_flights_within_k_stops.cpp
+++ graph/0787_cheapest_flights_within_k_stops.cpp
@@ -1,5 +1,5 @@
 #include <queue>
-#include <tuple>
+#include <unordered_map>
 #include <vector>
 
 /**
@@ -24,36 +24,81 @@
 class Solution
 {
 public:
-    int findCheapestPrice(int n, const std::vector<std::vector<int>>& flights, int src, int dst,
-                          int k)
+    int findCheapestPrice(int n, std::vector<std::vector<int>>& flights, int src, int dst, int k)
     {
-        std::vector<std::vector<std::pair<int, int>>> graph(n, std::vector<std::pair<int, int>>{});
-        for (const auto& flight : flights) {
-            const auto& from = flight[0];
-            const auto& to = flight[1];
-            const auto& price = flight[2];
-            graph[from].push_back({to, price});
+        return approach1(n, flights, src, dst, k);
+    }
+
+private:
+    int approach3(int n, std::vector<std::vector<int>>& flights, int src, int dst, int k)
+    {
+        // Bellman-Ford (DP with space optimization)
+        std::vector<int> dp(n, INT_MAX);
+        dp[src] = 0;
+        for (int i = 1; i <= k + 1; ++i) {
+            auto update = dp;
+            for (const auto& flight : flights) {
+                const auto& from = flight[0];
+                const auto& to = flight[1];
+                const auto& weight = flight[2];
+                if (dp[from] == INT_MAX)
+                    continue;
+
+                update[to] = std::min(update[to], dp[from] + weight);
+            }
+            dp = update;
         }
-        // tuple<int, int, int>,
-        // - tuple<2> = index of target vertex
-        // - tuple<0> = total price from src to this target
-        // - tuple<1> = edges from src to this target
-        using Tuple = std::tuple<int, int, int>;
-        std::priority_queue<Tuple, std::vector<Tuple>, std::greater<Tuple>> queue;
-        queue.push({0, 0, src});
-        std::vector<int> edgesTo(n, INT_MAX);
-        while (!queue.empty()) {
-            const auto [totalPrice, edges, v] = queue.top();
-            queue.pop();
-            if (edges >= edgesTo[v] || edges > k + 1)
+        return dp[dst] == INT_MAX ? -1 : dp[dst];
+    }
+
+    int approach2(int n, std::vector<std::vector<int>>& flights, int src, int dst, int k)
+    {
+        // DP
+        // - all edges have positive weights, no need to check negative cycle
+        // - at most k stops means at most k+1 edges
+        // dp[i][j] = min distance from src to j using at most i edges
+        std::vector<std::vector<int>> dp(k + 2, std::vector<int>(n, INT_MAX));
+        dp[0][src] = 0;
+        int result = INT_MAX;
+        for (int i = 1; i <= k + 1; ++i) {
+            for (const auto& flight : flights) {
+                const auto& from = flight[0];
+                const auto& to = flight[1];
+                const auto& weight = flight[2];
+                if (dp[i - 1][from] == INT_MAX)
+                    continue;
+
+                dp[i][to] = std::min(dp[i][to], dp[i - 1][from] + weight);
+            }
+            result = std::min(result, dp[i][dst]);
+        }
+        return result == INT_MAX ? -1 : result;
+    }
+
+    int approach1(int n, std::vector<std::vector<int>>& flights, int src, int dst, int k)
+    {
+        // Dijkstra
+        std::unordered_map<int, std::vector<std::pair<int, int>>> graph;
+        for (const auto& flight : flights) {
+            graph[flight[0]].push_back({flight[1], flight[2]}); // from:[<to,price>,...]
+        }
+        using Triple = std::tuple<int, int, int>; // <cost, steps, vertex>
+        std::priority_queue<Triple, std::vector<Triple>, std::greater<Triple>> pq;
+        pq.push({0, 0, src});
+        std::vector<int> stepsTo(n, INT_MAX);
+        stepsTo[src] = 0;
+        while (!pq.empty()) {
+            const auto [cost, steps, v] = pq.top();
+            pq.pop();
+            if (steps > k + 1 || steps > stepsTo[v])
                 continue;
 
             if (v == dst)
-                return totalPrice;
+                return cost;
 
-            edgesTo[v] = edges;
-            for (const auto& [adj, price] : graph[v]) {
-                queue.push({totalPrice + price, edges + 1, adj});
+            stepsTo[v] = steps;
+            for (const auto& [w, price] : graph[v]) {
+                pq.push({cost + price, steps + 1, w});
             }
         }
         return -1;
