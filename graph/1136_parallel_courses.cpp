455238e57aafb9e7de265e3dde2bd08d62e2c16f
mirinta <mirinta@163.com>
2023-07-07 15:47:31 +0800

    1136

---
+++ graph/1136_parallel_courses.cpp
@@ -0,0 +1,60 @@
+#include <queue>
+#include <vector>
+
+/**
+ * You are given an integer n, which indicates that there are n courses labeled from 1 to n. You are
+ * also given an array relations where relations[i] = [prevCourse_i, nextCourse_i], representing a
+ * prerequisite relationship between course prevCourse_i and course nextCourse_i: course
+ * prevCourse_i has to be taken before course nextCourse_i.
+ *
+ * In one semester, you can take any number of courses as long as you have taken all the
+ * prerequisites in the previous semester for the courses you are taking.
+ *
+ * Return the minimum number of semesters needed to take all courses. If there is no way to take all
+ * the courses, return -1.
+ *
+ * ! 1 <= n <= 5000
+ * ! 1 <= relations.length <= 5000
+ * ! relations[i].length == 2
+ * ! 1 <= prevCourse_i, nextCourse_i <= n
+ * ! prevCourse_i != nextCourse_i
+ * ! All the pairs [prevCourse_i, nextCourse_i] are unique.
+ */
+
+class Solution
+{
+public:
+    int minimumSemesters(int n, std::vector<std::vector<int>>& relations)
+    {
+        // topological sorting, Kahn's algorithm
+        std::vector<std::vector<int>> graph(n, std::vector<int>());
+        std::vector<int> indegrees(n, 0);
+        for (const auto& relation : relations) {
+            graph[relation[0] - 1].push_back(relation[1] - 1);
+            indegrees[relation[1] - 1]++;
+        }
+        std::queue<int> queue;
+        for (int i = 0; i < n; ++i) {
+            if (indegrees[i] == 0) {
+                queue.push(i);
+            }
+        }
+        int courses = 0;
+        int semesters = 0;
+        while (!queue.empty()) {
+            const int size = queue.size();
+            for (int k = 0; k < size; ++k) {
+                const int v = queue.front();
+                queue.pop();
+                courses++;
+                for (const auto& adj : graph[v]) {
+                    if (--indegrees[adj] == 0) {
+                        queue.push(adj);
+                    }
+                }
+            }
+            semesters++;
+        }
+        return courses == n ? semesters : -1;
+    }
+};
