2706f4ba6fe514e7a3a5c6bd23172dc5ffb51d2d
mirinta <mirinta@163.com>
2023-07-18 17:03:17 +0800

    Update 0206_reverse_linked_list.cpp

--- linked_list/0206_reverse_linked_list.cpp
+++ linked_list/0206_reverse_linked_list.cpp
@@ -1,3 +1,5 @@
+#include <stack>
+
 /**
  * Definition for singly-linked list.
  */
@@ -12,51 +14,66 @@ struct ListNode
 
 /**
  * Given the "head" of a singly linked list, reverse the list, and return the reversed list.
+ *
+ * ! The number of nodes in the list is the range [0, 5000].
+ * ! -5000 <= Node.val <= 5000
  */
 
 class Solution
 {
 public:
-    ListNode* reverseList(ListNode* head)
+    ListNode* reverseList(ListNode* head) { return approach3(head); }
+
+private:
+    // iteration
+    ListNode* approach3(ListNode* head)
     {
-        // approach 1: iteration
+        if (!head)
+            return nullptr;
+
         ListNode* prev = nullptr;
-        auto* iter = head;
-        while (iter) {
-            auto* next = iter->next;
-            iter->next = prev;
-            prev = iter;
-            iter = next;
+        auto* current = head;
+        while (current) {
+            auto* next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
         }
         return prev;
-        // approach 2: recursion
-        // 1. N0 -> reverseList(N1 -> ... -> Nm)
-        // 2. N0 -> (N1 <- ... <- Nm), Nm is the head of the reversed sub-list
-        // 3. Set N0->next->next = N0 and N0->next = nullptr;
-        // if (!head || !head->next)
-        //     return head;
+    }
 
-        // auto* newHead = reverseList(head->next);
-        // head->next->next = head;
-        // head->next = nullptr;
-        // return newHead;
-        // ------------------------------
-        // approach 3: stack
-        // std::stack<ListNode*> stack;
-        // auto* iter = head;
-        // while (iter) {
-        //     stack.push(iter);
-        //     auto* temp = iter;
-        //     iter = iter->next;
-        //     temp->next = nullptr;
-        // }
-        // ListNode resultVHead{};
-        // iter = &resultVHead;
-        // while (!stack.empty()) {
-        //     iter->next = stack.top();
-        //     iter = iter->next;
-        //     stack.pop();
-        // }
-        // return resultVHead.next;
+    // recursion
+    ListNode* approach2(ListNode* head)
+    {
+        if (!head || !head->next)
+            return head;
+
+        // 0->[1<-2<-...]
+        auto* newHead = approach2(head->next);
+        head->next->next = head;
+        head->next = nullptr;
+        return newHead;
+    }
+
+    // stack
+    ListNode* approach1(ListNode* head)
+    {
+        if (!head)
+            return nullptr;
+
+        std::stack<ListNode*> stack;
+        for (auto* i = head; i; i = i->next) {
+            stack.push(i);
+        }
+        ListNode vHead(-1);
+        auto* i = &vHead;
+        while (!stack.empty()) {
+            auto* next = stack.top()->next;
+            stack.top()->next = nullptr;
+            i->next = stack.top();
+            i = i->next;
+            stack.pop();
+        }
+        return vHead.next;
     }
 };
