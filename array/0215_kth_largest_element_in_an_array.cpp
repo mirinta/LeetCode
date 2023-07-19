88544faf44057ad250f6db2cd9341b0ea90bd6d5
mirinta <mirinta@163.com>
2023-07-19 17:47:15 +0800

    Update 0215_kth_largest_element_in_an_array.cpp

--- array/0215_kth_largest_element_in_an_array.cpp
+++ array/0215_kth_largest_element_in_an_array.cpp
@@ -1,3 +1,4 @@
+#include <queue>
 #include <random>
 #include <vector>
 
@@ -6,49 +7,63 @@
  *
  * Note that it is the "k"th largest element in the sorted order, not the "k"th distinct element.
  *
- * You must solve it in O(n) time complexity.
+ * Can you solve it without sorting?
+ *
+ * ! 1 <= k <= nums.length <= 10^5
+ * ! -10^4 <= nums[i] <= 10^4
  */
 
 class Solution
 {
 public:
-    int findKthLargest(std::vector<int>& nums, int k)
-    {
-        if (nums.empty())
-            return -1;
+    int findKthLargest(std::vector<int>& nums, int k) { return approach1(nums, k); }
 
+private:
+    // priority-queue, time O(nlogk), space O(k)
+    int approach2(std::vector<int>& nums, int k)
+    {
+        std::priority_queue<int, std::vector<int>, std::greater<int>> pq; // min heap
+        for (const auto& val : nums) {
+            pq.push(val);
+            while (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        return pq.top();
+    }
+
+    // quick-select, worst time O(n^2), average time O(n), space O(1)
+    int approach1(std::vector<int>& nums, int k)
+    {
         std::random_device rd;
         std::mt19937 g(rd());
         std::shuffle(nums.begin(), nums.end(), g);
-        int low = 0;
-        int high = nums.size() - 1;
-        // k is 1-indexed
-        int rank = nums.size() - k; // 0-indexed, "rank + 1"th smallest = "k"th largest
-        while (low <= high) {
-            int pos = partition(nums, low, high);
-            if (pos == rank)
-                return nums[pos];
-
+        const int rank = nums.size() - k;
+        int lo = 0;
+        int hi = nums.size() - 1;
+        while (lo <= hi) {
+            const int pos = partition(lo, hi, nums);
             if (pos > rank) {
-                high = pos - 1;
+                hi = pos - 1;
+            } else if (pos < rank) {
+                lo = pos + 1;
             } else {
-                low = pos + 1;
+                return nums[pos];
             }
         }
         return -1;
     }
 
-private:
-    int partition(std::vector<int>& nums, int low, int high)
+    int partition(int lo, int hi, std::vector<int>& nums)
     {
-        int pivot = nums[low];
-        int i = low + 1;
-        int j = high;
+        const int pivot = nums[lo];
+        int i = lo + 1;
+        int j = hi;
         while (true) {
-            while (i <= high && nums[i] <= pivot) {
+            while (i <= hi && nums[i] <= pivot) {
                 i++;
             }
-            while (j >= low && nums[j] > pivot) {
+            while (j >= lo && nums[j] > pivot) {
                 j--;
             }
             if (i >= j)
@@ -56,7 +71,7 @@ private:
 
             std::swap(nums[i], nums[j]);
         }
-        std::swap(nums[low], nums[j]);
+        std::swap(nums[j], nums[lo]);
         return j;
     }
 };
