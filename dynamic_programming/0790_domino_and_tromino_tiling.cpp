fb3ce870d833119aa9a2d2dd18b497c40794b982
mirinta <mirinta@163.com>
2023-07-20 11:26:31 +0800

    Update 0790_domino_and_tromino_tiling.cpp

--- dynamic_programming/0790_domino_and_tromino_tiling.cpp
+++ dynamic_programming/0790_domino_and_tromino_tiling.cpp
@@ -16,31 +16,27 @@ class Solution
 public:
     int numTilings(int n)
     {
-        if (n <= 0)
-            return 0;
-
-        // dp[i][j] = number of ways to tile an 2xi board
-        // - j is either 0, 1, or 2
-        // - case 1: j = 0, both rows of the ith column are covered
-        // - dp[i][0] = dp[i - 1][0] + dp[i - 2][0] + dp[i - 1][2] + dp[i - 1][1];
-        // X X
-        // X X
-        // - case 2: j = 1, only the 1st row of the ith column is covered
-        // - dp[i][0] = dp[i - 1][0] + dp[i - 2][0] + dp[i - 1][2] + dp[i - 1][1];
-        // X X
-        // X
-        // - case 3: j = 2, only the 2nd row of the ith column is covered
-        // - dp[i][2] = dp[i - 2][0] + dp[i - 1][1];
-        // X
-        // X X
-        // - finally, case 2 and case 3 are the same
-        std::vector<std::vector<long long>> dp(n + 1, std::vector<long long>(2, 0));
+        // dp[i][j] = num of ways to tile an 2xi board
+        // j = 0   j = 1   j = 2
+        //  X X      X X     X
+        //  X X      X       X X
+        // j = 0 means both rows are covered
+        // j = 1 means only the first row is covered
+        // j = 2 means only the second row is covered
+        constexpr int kMod = 1e9 + 7;
+        std::vector<std::vector<long>> dp(n + 1, std::vector<long>(3, 0));
         dp[0][0] = 1;
         dp[1][0] = 1;
-        constexpr int mod = 1e9 + 7;
         for (int i = 2; i <= n; ++i) {
-            dp[i][0] = (dp[i - 1][0] + dp[i - 2][0] + 2 * dp[i - 1][1]) % mod;
-            dp[i][1] = (dp[i - 2][0] + dp[i - 1][1]) % mod;
+            // X X D    X X T    X T T    X D D
+            // X X D    X T T    X X T    X D D
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2] + dp[i - 2][0]) % kMod;
+            // X T T    X D D
+            // X T      X X
+            dp[i][1] = (dp[i - 2][0] + dp[i - 1][2]) % kMod;
+            // X T      X X
+            // X T T    X D D
+            dp[i][2] = (dp[i - 2][0] + dp[i - 1][1]) % kMod;
         }
         return dp[n][0];
     }
