1508cbf163f0f531ec8d52dfefc89aa9117ed9b8
mirinta <mirinta@163.com>
2023-07-20 14:57:07 +0800

    Update 0072_edit_distance.cpp

--- dynamic_programming/double_sequences/0072_edit_distance.cpp
+++ dynamic_programming/double_sequences/0072_edit_distance.cpp
@@ -17,34 +17,67 @@
 class Solution
 {
 public:
-    int minDistance(std::string word1, std::string word2)
+    int minDistance(std::string word1, std::string word2) { return approach2(word1, word2); }
+
+private:
+    int approach2(const std::string& word1, const std::string& word2)
     {
-        // dp[i][j] = minDistance(word[0: i), word2[0: j))
-        const auto m = word1.size();
-        const auto n = word2.size();
+        // DP with space optimization
+        if (word1.empty())
+            return word2.size();
+
+        const int m = word1.size();
+        const int n = word2.size();
+        std::vector<int> iMinus1(n + 1, 0);
+        for (int i = 1; i <= n; ++i) {
+            iMinus1[i] = i;
+        }
+        std::vector<int> dp(n + 1, 0);
+        for (int i = 1; i <= m; ++i) {
+            dp[0] = i;
+            for (int j = 1; j <= n; ++j) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[j] = iMinus1[j - 1];
+                    continue;
+                }
+                const int insertion = dp[j - 1] + 1;
+                const int swap = iMinus1[j - 1] + 1;
+                const int deletion = iMinus1[j] + 1;
+                dp[j] = std::min({insertion, swap, deletion});
+            }
+            iMinus1 = dp;
+        }
+        return dp[n];
+    }
+
+    int approach1(const std::string& word1, const std::string& word2)
+    {
+        const int m = word1.size();
+        const int n = word2.size();
+        // dp[i][j] = min distance to covert word1[0:i) to word2[0:j)
         std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
         // base cases:
-        // - dp[0][j], insert each char of word2[0: j), dp[0][j] = j
-        // - dp[i][0], delete each char of word1[0: i), dp[i][0] = i
-        for (int i = 0; i <= m; ++i) {
-            dp[i][0] = i;
-        }
-        for (int j = 0; j <= n; ++j) {
+        // dp[0][0] = 0, both word1 and word2 are empty, no operation
+        // dp[0][j] = j, word1 is empty, insert all characters of word2 to word1
+        // dp[i][0] = i, word2 is empty, delete all characters of word1
+        for (int j = 1; j <= n; ++j) {
             dp[0][j] = j;
         }
         for (int i = 1; i <= m; ++i) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
+                // X X X i
+                // Y Y Y j
                 if (word1[i - 1] == word2[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1];
                     continue;
                 }
-                // X X [X] i
-                // Y Y [Y] j
-                // three choices: insert, delete, and replace
-                // - insert char at i: distance = dp[i][j - 1] + 1, move pointer j only
-                // - delete char at i: distance = d[i - 1][j] + 1, move pointer i only
-                // - replace char at i: distance = dp[i - 1][j - 1] + 1, move both pointers
-                dp[i][j] = 1 + std::min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]});
+                const int insertion = dp[i][j - 1] + 1;
+                const int swap = dp[i - 1][j - 1] + 1;
+                const int deletion = dp[i - 1][j] + 1;
+                dp[i][j] = std::min({insertion, swap, deletion});
             }
         }
         return dp[m][n];
