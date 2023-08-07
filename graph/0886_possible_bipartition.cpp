d29ee8540e92c66d5cc3edede6d2c6c143adc32f
mirinta <mirinta@163.com>
2023-08-08 01:43:07 +0800

    Update 0886_possible_bipartition.cpp

--- graph/0886_possible_bipartition.cpp
+++ graph/0886_possible_bipartition.cpp
@@ -1,3 +1,4 @@
+#include <queue>
 #include <vector>
 
 /**
@@ -7,6 +8,12 @@
  * Given the integer "n" and the array "dislikes" where dislike[i] = [ai, bi] indicates that the
  * person labeled ai does not like the person labeled bi, return true if it is possible to split
  * everyone into two groups in this way.
+ *
+ * ! 1 <= n <= 2000
+ * ! 0 <= dislikes.length <= 10^4
+ * ! dislikes[i].length == 2
+ * ! 1 <= ai < bi <= n
+ * ! All the pairs of dislikes are unique.
  */
 
 class Solution
@@ -14,48 +21,69 @@ class Solution
 public:
     bool possibleBipartition(int n, std::vector<std::vector<int>>& dislikes)
     {
-        // build undirected graph
-        std::vector<std::vector<int>> graph(n, std::vector<int>{});
-        for (const auto& pair : dislikes) {
-            // pi = [ai, bi], ai and bi are 1-indexed
-            const auto from = pair[0] - 1;
-            const auto to = pair[1] - 1;
-            graph[from].push_back(to);
-            graph[to].push_back(from);
+        std::vector<std::vector<int>> graph(n);
+        for (const auto& dislike : dislikes) {
+            graph[dislike[0] - 1].push_back(dislike[1] - 1);
+            graph[dislike[1] - 1].push_back(dislike[0] - 1);
         }
-        // initialize
-        result = true;
-        visited = std::vector<bool>(n, false);
-        color = std::vector<bool>(n, false);
-        for (size_t s = 0; s < graph.size(); ++s) {
-            if (!visited[s]) {
-                dfs(graph, s);
-            }
-        }
-        return result;
+        return approach2(graph);
     }
 
 private:
-    bool result = true;
-    std::vector<bool> visited;
-    std::vector<bool> color;
-
-    void dfs(const std::vector<std::vector<int>>& graph, int s)
+    // BFS: time O(V+E), space O(V+E)
+    bool approach2(const std::vector<std::vector<int>>& graph)
     {
-        if (!result)
-            return;
+        const int n = graph.size();
+        std::vector<int> colors(n, 0); // 0 or not visited, -1 for blue, 1 for red
+        std::queue<int> queue;
+        for (int i = 0; i < n; ++i) {
+            if (colors[i] != 0)
+                continue;
 
-        visited[s] = true;
-        for (const auto& adj : graph[s]) {
-            if (!visited[adj]) {
-                color[adj] = !color[s];
-                dfs(graph, adj);
-            } else {
-                if (color[adj] == color[s]) {
-                    result = false;
-                    return;
+            queue.push(i);
+            colors[i] = 1;
+            while (!queue.empty()) {
+                const auto v = queue.front();
+                queue.pop();
+                for (const auto& adj : graph[v]) {
+                    if (colors[adj] == 0) {
+                        colors[adj] = -colors[v];
+                        queue.push(adj);
+                    } else if (colors[adj] == colors[v])
+                        return false;
                 }
             }
         }
+        return true;
+    }
+
+    // DFS, time O(V+E), space O(V+E)
+    bool approach1(const std::vector<std::vector<int>>& graph)
+    {
+        const int n = graph.size();
+        std::vector<int> colors(n, 0); // 0 or not visited, -1 for blue, 1 for red
+        for (int i = 0; i < n; ++i) {
+            if (colors[i] != 0)
+                continue;
+
+            colors[i] = 1;
+            if (!dfs(colors, i, graph))
+                return false;
+        }
+        return true;
+    }
+
+    bool dfs(std::vector<int>& colors, int v, const std::vector<std::vector<int>>& graph)
+    {
+        for (const auto& adj : graph[v]) {
+            if (colors[adj] == 0) {
+                colors[adj] = -colors[v];
+                if (!dfs(colors, adj, graph))
+                    return false;
+
+            } else if (colors[adj] == colors[v])
+                return false;
+        }
+        return true;
     }
 };
