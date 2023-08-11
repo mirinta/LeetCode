3ad3d030a8acdf2649cc472d2f2d3bd411820d02
mirinta <mirinta@163.com>
2023-08-11 11:16:58 +0800

    Update 0518_coin_change.cpp

--- dynamic_programming/knapsack/0518_coin_change.cpp
+++ dynamic_programming/knapsack/0518_coin_change.cpp
@@ -11,26 +11,42 @@
  *
  * The answer is guaranteed to fit into a signed 32-bit integer.
  *
- * Example:
- * Input: amount = 5, coins = [1, 2, 5]
- * Output: 4
- * Explanation: there are four ways to make the amount:
- * 5 = 5
- * 5 = 2 + 2 + 1
- * 5 = 2 + 1 + 1 + 1
- * 5 = 1 + 1 + 1 + 1
+ * ! 1 <= coins.length <= 300
+ * ! 1 <= coins[i] <= 5000
+ * ! All the values of coins are unique.
+ * ! 0 <= amount <= 5000
  */
 
 class Solution
 {
 public:
-    int change(int amount, std::vector<int>& coins)
+    int change(int amount, std::vector<int>& coins) { return approach2(amount, coins); }
+
+private:
+    int approach2(int amount, std::vector<int>& coins)
+    {
+        // DP with space optimization
+        const int n = coins.size();
+        std::vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= amount; ++j) {
+                if (j - coins[i - 1] < 0)
+                    continue;
+
+                dp[j] += dp[j - coins[i - 1]];
+            }
+        }
+        return dp[amount];
+    }
+
+    int approach1(int amount, std::vector<int>& coins)
     {
         // consider it as a knapsack problem:
         // how many ways to fill a knapsack of capacity M with N packages?
         // - each package has a value representing the capacity it occupies
         // dp[i][j] = number of ways that make up amount j with coins[0:i)
-        const auto n = coins.size();
+        const int n = coins.size();
         std::vector<std::vector<int>> dp(n + 1, std::vector<int>(amount + 1, 0));
         // base case:
         // - dp[i][0] = 1, there's only one way to make up 0 amount: choose nothing
@@ -47,7 +63,6 @@ public:
                 } else {
                     // here, we can either pick or not pick the ith coin
                     // - if the ith coin is picked, then the coin range is expanded to [0:i),
-                    //   because each coin can be used multiple times
                     //   our goal is to make up the remaining amount r = j - denomination of the ith
                     //   coin, we have dp[i][r] ways to achieve the goal
                     // - otherwise, the coin list is still [0:i-1), and the goal is still j,
