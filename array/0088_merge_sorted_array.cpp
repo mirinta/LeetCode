bb7f283592043ce04e41a60454823c19be6671f3
mirinta <mirinta@163.com>
2023-08-11 16:51:04 +0800

    Update 0088_merge_sorted_array.cpp

--- array/0088_merge_sorted_array.cpp
+++ array/0088_merge_sorted_array.cpp
@@ -10,6 +10,14 @@
  * array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote
  * the elements that should be merged, and the last n elements are set to 0 and should be ignored.
  * nums2 has a length of n.
+ *
+ * ! nums1.length == m + n
+ * ! nums2.length == n
+ * ! 0 <= m, n <= 200
+ * ! 1 <= m + n <= 200
+ * ! -10^9 <= nums1[i], nums2[j] <= 10^9
+ *
+ * ! Can you come up with an algorithm that runs in O(m + n) time?
  */
 
 class Solution
@@ -17,6 +25,8 @@ class Solution
 public:
     void merge(std::vector<int>& nums1, int m, std::vector<int>& nums2, int n)
     {
+        // X X X ... X X X X ... X
+        // |<-nums1->| |<-empty->|
         for (int k = m + n - 1, i = m - 1, j = n - 1; k >= 0; --k) {
             if (i < 0 && j < 0)
                 break;
