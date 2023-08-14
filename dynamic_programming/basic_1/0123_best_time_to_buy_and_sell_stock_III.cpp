56237870a393eae49c0fb423ddf58aa214f5fdc1
mirinta <mirinta@163.com>
2023-08-14 12:47:19 +0800

    Update 0123_best_time_to_buy_and_sell_stock_III.cpp

--- dynamic_programming/basic_1/0123_best_time_to_buy_and_sell_stock_III.cpp
+++ dynamic_programming/basic_1/0123_best_time_to_buy_and_sell_stock_III.cpp
@@ -7,35 +7,56 @@
  *
  * Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock
  * before you buy again).
+ *
+ * ! 1 <= prices.length <= 10^5
+ * ! 0 <= prices[i] <= 10^5
  */
 
 class Solution
 {
 public:
-    int maxProfit(const std::vector<int>& prices)
-    {
-        if (prices.empty())
-            return 0;
+    int maxProfit(std::vector<int>& prices) { return approach2(prices); }
 
-        // dp[i][k][0] = max profit of days[0:i) with k buy-in's and without stock on the ith day
-        // dp[i][k][1] = max profit of days[0:i) with k buy-in's and wit stock on the ith day
-        // NOTE:
-        // Each transaction starts with a buy-in and ends with a sell-out.
-        // Therefore, limiting the number of transactions is equivalent to limiting the number of
-        // buy-in actions.
-        const auto n = prices.size();
-        const auto maxTransactions = 2;
-        std::vector<std::vector<std::vector<int>>> dp(
-            n + 1, std::vector<std::vector<int>>(maxTransactions + 1, std::vector<int>(2, 0)));
-        for (int k = 0; k <= maxTransactions; ++k) {
-            dp[0][k][1] = INT_MIN;
+private:
+    // DP with space optimization, time O(NK), space O(K)
+    int approach2(const std::vector<int>& prices)
+    {
+        const int n = prices.size();
+        const int k = 2;
+        std::vector<std::vector<int>> dp(k + 1, std::vector<int>(2, 0));
+        for (int j = 0; j <= k; ++j) {
+            dp[j][1] = INT_MIN;
         }
-        for (int i = 1; i <= n; ++i) {
-            for (int k = 1; k <= maxTransactions; ++k) {
-                dp[i][k][0] = std::max(dp[i - 1][k][0], dp[i - 1][k][1] + prices[i - 1]);
-                dp[i][k][1] = std::max(dp[i - 1][k][1], dp[i - 1][k - 1][0] - prices[i - 1]);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 1; j <= k; ++j) {
+                dp[j][0] = std::max(dp[j][0], dp[j][1] + prices[i]);
+                dp[j][1] = std::max(dp[j][1], dp[j - 1][0] - prices[i]);
             }
         }
-        return dp[n][maxTransactions][0];
+        return std::max(dp[k][0], dp[k][1]);
+    }
+
+    // DP, time O(NK), space O(NK)
+    int approach1(const std::vector<int>& prices)
+    {
+        const int n = prices.size();
+        const int k = 2;
+        // each transaction starts with a buy-in and ends with a sell-out,
+        // so limiting the num of transactions is equivalent to limiting the num of buy-in's
+        // dp[i][j][0] = max profit at the end of ith day without any stock in hand, and with at
+        // most j buy-in's dp[i][j][1] = max profit at the end of ith day with one stock in hand,
+        // and with at most j buy-in's
+        std::vector<std::vector<std::vector<int>>> dp(
+            n + 1, std::vector<std::vector<int>>(k + 1, std::vector<int>(2, 0)));
+        for (int j = 0; j <= k; ++j) {
+            dp[0][j][1] = INT_MIN;
+        }
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= k; ++j) {
+                dp[i][j][0] = std::max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i - 1]);
+                dp[i][j][1] = std::max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i - 1]);
+            }
+        }
+        return std::max(dp[n][k][0], dp[n][k][1]);
     }
 };
