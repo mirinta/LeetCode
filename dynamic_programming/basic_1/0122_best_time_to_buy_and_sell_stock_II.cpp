227114222361e3cddd57ae5182f100d89a81d4c3
mirinta <mirinta@163.com>
2023-08-14 12:05:29 +0800

    Update 0122_best_time_to_buy_and_sell_stock_II.cpp

--- dynamic_programming/basic_1/0122_best_time_to_buy_and_sell_stock_II.cpp
+++ dynamic_programming/basic_1/0122_best_time_to_buy_and_sell_stock_II.cpp
@@ -16,41 +16,35 @@
 class Solution
 {
 public:
-    int maxProfit(const std::vector<int>& prices) { return approach2(prices); }
+    int maxProfit(std::vector<int>& prices) { return approach2(prices); }
 
 private:
+    // 1D-DP, time O(N), space O(1)
     int approach2(const std::vector<int>& prices)
     {
-        // space optimization of approach1
-        if (prices.empty())
-            return 0;
-
-        const auto n = prices.size();
+        const int n = prices.size();
         int withStock = INT_MIN;
         int withoutStock = 0;
-        for (const auto& price : prices) {
-            int backupWithoutStock = withoutStock;
-            withoutStock = std::max(withoutStock, withStock + price);
-            withStock = std::max(withStock, backupWithoutStock - price);
+        for (int i = 0; i < n; ++i) {
+            const int backup = withStock;
+            withStock = std::max(withStock, withoutStock - prices[i]);
+            withoutStock = std::max(withoutStock, backup + prices[i]);
         }
-        return withoutStock;
+        return std::max(withStock, withoutStock);
     }
 
+    // 2D-DP, time O(N), space O(N)
     int approach1(const std::vector<int>& prices)
     {
-        if (prices.empty())
-            return 0;
-
-        // dp[i][0] = max profit of days[0:i) without stock on the ith day
-        // dp[i][j] = max profit of days[0:i) with stock on the ith day
-        const auto n = prices.size();
+        const int n = prices.size();
+        // dp[i][0] = max profit at the end of ith day without any stock in-hand
+        // dp[i][1] = max profit at the end of ith day with one stock in-had
         std::vector<std::vector<int>> dp(n + 1, std::vector<int>(2, 0));
         dp[0][1] = INT_MIN;
         for (int i = 1; i <= n; ++i) {
-            // buy and sell stock on the same day, profit = 0
             dp[i][0] = std::max(dp[i - 1][0], dp[i - 1][1] + prices[i - 1]);
             dp[i][1] = std::max(dp[i - 1][1], dp[i - 1][0] - prices[i - 1]);
         }
-        return dp[n][0];
+        return std::max(dp[n][0], dp[n][1]);
     }
 };
