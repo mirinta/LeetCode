c0b1ec84cfe903761e36a5dec16dcb18041b8b3c
mirinta <mirinta@163.com>
2023-08-15 19:35:38 +0800

    Update 0070_climbing_stairs.cpp

--- dynamic_programming/basic_1/0070_climbing_stairs.cpp
+++ dynamic_programming/basic_1/0070_climbing_stairs.cpp
@@ -1,44 +1,46 @@
-#include <utility>
+#include <vector>
 
 /**
  * You are climbing a staircase. It takes "n" steps to reach the top.
  *
  * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ * ! 1 <= n <= 45
  */
 
 class Solution
 {
 public:
-    int climbStairs(int n)
-    {
-        if (n < 1)
-            return 0;
+    int climbStairs(int n) { return approach2(n); }
 
-        if (n == 1)
+private:
+    // DP with space optimization, time O(N), space O(1)
+    int approach2(int n)
+    {
+        if (n < 2)
             return 1;
 
-        // approach 1: 1D array DP
-        // dp[i] = distinct ways of climbing i steps to reach the top
-        // std::vector<int> dp(n + 1, 0);
-        // dp[1] = 1;
-        // dp[2] = 2;
-        // for (int i = 3; i <= n; ++i) {
-        //     dp[i] = dp[i - 1] + dp[i - 2];
-        // }
-        // return dp[n];
-
-        // approach 2:
-        // we see that dp[i] is only dependent on dp[i-1] and dp[i-2],
-        // instead of using an array, we can only maintain these two values
-        int dp_2 = 1; // dp(n-2)
-        int dp_1 = 2; // dp(n-1)
-        for (int i = 3; i <= n; ++i) {
-            // prepare for the next round:
-            // dp(n+1) = dp(n) + dp(n-1),
-            // dp_1 = dp(n), dp_2 = dp(n-1)
-            std::swap(dp_1, dp_2);
-            dp_1 += dp_2;
+        int iMinus1 = 1;
+        int iMinus2 = 1;
+        int result = 0;
+        for (int i = 2; i <= n; ++i) {
+            result = iMinus1 + iMinus2;
+            iMinus2 = iMinus1;
+            iMinus1 = result;
         }
-        return dp_1;
+        return result;
+    }
+
+    // DP, time O(N), space O(N)
+    int approach1(int n)
+    {
+        // dp[i] = num of distinct ways to reach the ith floor
+        std::vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
