4dab1571167d4d5909ee6840a2c45190cc171385
mirinta <mirinta@163.com>
2023-08-16 16:54:04 +0800

    Add files via upload

---
+++ priority_queue/0502_IPO.cpp
@@ -0,0 +1,59 @@
+#include <queue>
+#include <vector>
+
+/**
+ * Suppose LeetCode will start its IPO soon. In order to sell a good price of its shares to Venture
+ * Capital, LeetCode would like to work on some projects to increase its capital before the IPO.
+ * Since it has limited resources, it can only finish at most k distinct projects before the IPO.
+ * Help LeetCode design the best way to maximize its total capital after finishing at most k
+ * distinct projects.
+ *
+ * You are given n projects where the ith project has a pure profit profits[i] and a minimum capital
+ * of capital[i] is needed to start it.
+ *
+ * Initially, you have w capital. When you finish a project, you will obtain its pure profit and the
+ * profit will be added to your total capital.
+ *
+ * Pick a list of at most k distinct projects from given projects to maximize your final capital,
+ * and return the final maximized capital.
+ *
+ * The answer is guaranteed to fit in a 32-bit signed integer.
+ *
+ * ! 1 <= k <= 10^5
+ * ! 0 <= w <= 10^9
+ * ! n == profits.length
+ * ! n == capital.length
+ * ! 1 <= n <= 10^5
+ * ! 0 <= profits[i] <= 10^4
+ * ! 0 <= capital[i] <= 10^9
+ */
+
+class Solution
+{
+public:
+    // time O(NlogN+KlogN), space O(N), N is the number of projects
+    int findMaximizedCapital(int k, int w, std::vector<int>& profits, std::vector<int>& capital)
+    {
+        std::vector<std::pair<int, int>> projects(capital.size()); // <capital, profit>
+        for (int i = 0; i < capital.size(); ++i) {
+            projects.push_back({capital[i], profits[i]});
+        }
+        std::sort(projects.begin(), projects.end(),
+                  [](const auto& p1, const auto& p2) { return p1.first < p2.first; });
+        int i = 0;
+        std::priority_queue<int> pq; // max heap
+        while (k) {
+            while (i < projects.size() && projects[i].first <= w) {
+                pq.push(projects[i].second);
+                i++;
+            }
+            if (pq.empty())
+                break;
+
+            w += pq.top();
+            pq.pop();
+            k--;
+        }
+        return w;
+    }
+};
