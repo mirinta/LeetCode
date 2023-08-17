ba7e229d81f11e4d2818c71c68a8a8dbc513b061
mirinta <mirinta@163.com>
2023-08-17 17:22:09 +0800

    Update 0383_ransom_note.cpp

--- string/0383_ransom_note.cpp
+++ string/0383_ransom_note.cpp
@@ -6,22 +6,23 @@
  * by using the letters from "magazine" and "false" otherwise.
  *
  * Each letter in "magazine" can only be used once in "ransomNote".
+ *
+ * ! 1 <= ransomNote.length, magazine.length <= 10^5
+ * ! ransomNote and magazine consist of lowercase English letters.
  */
 
 class Solution
 {
 public:
-    bool canConstruct(const std::string& ransomNote, const std::string& magazine)
+    bool canConstruct(std::string ransomNote, std::string magazine)
     {
-        std::unordered_map<char, size_t> freq;
+        std::unordered_map<char, int> freq;
         for (const auto& c : magazine) {
             freq[c]++;
         }
         for (const auto& c : ransomNote) {
-            if (!freq.count(c) || freq[c] == 0)
+            if (!freq.count(c) || --freq[c] < 0)
                 return false;
-
-            freq[c]--;
         }
         return true;
     }
