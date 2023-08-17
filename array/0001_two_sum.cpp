92ffc15d370f45db6c34963530f6c8d0dcba4159
mirinta <mirinta@163.com>
2023-08-17 19:44:03 +0800

    Update 0001_two_sum.cpp

--- array/0001_two_sum.cpp
+++ array/0001_two_sum.cpp
@@ -9,23 +9,27 @@
  * element twice.
  *
  * You can return the answer in any order.
+ *
+ * ! 2 <= nums.length <= 10^4
+ * ! -10^9 <= nums[i] <= 10^9
+ * ! -10^9 <= target <= 10^9
+ * ! Only one valid answer exists.
  */
 
 class Solution
 {
 public:
-    std::vector<int> twoSum(const std::vector<int>& nums, int target)
+    std::vector<int> twoSum(std::vector<int>& nums, int target)
     {
-        if (nums.empty() || nums.size() < 2)
-            return {};
-
         std::unordered_map<int, int> map; // value to index
+        // there's exactly one solution, it implies that all values are unique
         for (int i = 0; i < nums.size(); ++i) {
-            const auto diff = target - nums[i];
-            if (map.count(diff))
-                return {i, map[diff]};
-
-            map[nums[i]] = i;
+            const int diff = target - nums[i];
+            if (!map.count(diff)) {
+                map[nums[i]] = i;
+            } else {
+                return {map[diff], i};
+            }
         }
         return {};
     }
