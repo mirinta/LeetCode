7edcce24f8fcca678bcdc95771eb193f46324bb8
mirinta <mirinta@163.com>
2023-08-18 11:24:05 +0800

    Update 0228_summary_ranges.cpp

--- array/0228_summary_ranges.cpp
+++ array/0228_summary_ranges.cpp
@@ -20,43 +20,30 @@
  * ! -2^31 <= nums[i] <= 2^31 - 1
  * ! All the values of nums are unique.
  * ! nums is sorted in ascending order.
- *
- * Example:
- * Input: nums = [0,2,3,4,6,8,9]
- * Output: ["0","2->4","6","8->9"]
- * Explanation: The ranges are:
- * [0,0] --> "0"
- * [2,4] --> "2->4"
- * [6,6] --> "6"
- * [8,9] --> "8->9"
  */
 
 class Solution
 {
 public:
-    std::vector<std::string> summaryRanges(const std::vector<int>& nums)
+    std::vector<std::string> summaryRanges(std::vector<int>& nums)
     {
         if (nums.empty())
             return {};
 
-        if (nums.size() == 1)
-            return {std::to_string(nums[0])};
-
+        const int n = nums.size();
         std::vector<std::string> result;
-        for (int i = 0; i < nums.size(); ++i) {
-            int start = nums[i];
-            // 0,  1,  2,  4,  5,  7
-            // ^       ^
-            // start   i
-            while (i + 1 < nums.size() && nums[i] + 1 == nums[i + 1]) {
-                i++;
+        int i = 0;
+        while (i < n) {
+            int j = i + 1;
+            while (j < n && nums[j - 1] + 1 == nums[j]) {
+                j++;
             }
-            std::string s = std::to_string(start);
-            if (start != nums[i]) {
-                s.append("->");
-                s.append(std::to_string(nums[i]));
+            if (j - 1 == i) {
+                result.push_back(std::to_string(nums[i]));
+            } else {
+                result.push_back(std::to_string(nums[i]) + "->" + std::to_string(nums[j - 1]));
             }
-            result.push_back(std::move(s));
+            i = j;
         }
         return result;
     }
