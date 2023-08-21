3bbc23b0f884c4f10fc01a174ec5272f7fdcc89a
mirinta <mirinta@163.com>
2023-08-21 12:10:46 +0800

    Update 0399_evaluate_division.cpp

--- graph/0399_evaluate_division.cpp
+++ graph/0399_evaluate_division.cpp
@@ -16,11 +16,15 @@
  * Note: The input is always valid. You may assume that evaluating the queries will not result in
  * division by zero and that there is no contradiction.
  *
- * Input: equations = [["a","b"],["b","c"]], values = [2.0,3.0],
- * queries = [["a","c"],["b","a"],["a","e"],["a","a"],["x","x"]]
- * Output: [6.00000,0.50000,-1.00000,1.00000,-1.00000]
- * Explanation: Given: a / b = 2.0, b / c = 3.0 queries are:
- * a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? return: [6.0, 0.5, -1.0, 1.0, -1.0 ]
+ * ! 1 <= equations.length <= 20
+ * ! equations[i].length == 2
+ * ! 1 <= Ai.length, Bi.length <= 5
+ * ! values.length == equations.length
+ * ! 0.0 < values[i] <= 20.0
+ * ! 1 <= queries.length <= 20
+ * ! queries[i].length == 2
+ * ! 1 <= Cj.length, Dj.length <= 5
+ * ! Ai, Bi, Cj, Dj consist of lower case English letters and digits.
  */
 
 class Solution
@@ -30,54 +34,49 @@ public:
                                      std::vector<double>& values,
                                      std::vector<std::vector<std::string>>& queries)
     {
-        BiGraph graph;
+        Graph graph;
         for (int i = 0; i < equations.size(); ++i) {
-            const auto& numerator = equations[i][0];
-            const auto& denominator = equations[i][1];
-            const auto& quotient = values[i];
-            graph[numerator][denominator] = quotient;
-            graph[denominator][numerator] = 1 / quotient;
+            const auto& dividend = equations[i][0];
+            const auto& divisor = equations[i][1];
+            graph[dividend][divisor] = values[i];
+            graph[divisor][dividend] = 1 / values[i];
         }
-        std::vector<double> result;
         std::unordered_set<std::string> visited;
+        std::vector<double> result;
         for (const auto& query : queries) {
-            const auto& numerator = query[0];
-            const auto& denominator = query[1];
-            if (!graph.count(numerator) || !graph.count(denominator)) {
+            const auto& dividend = query[0];
+            const auto& divisor = query[1];
+            if (!graph.count(dividend) || !graph.count(divisor)) {
                 result.push_back(-1);
-                continue;
-            }
-            if (numerator == denominator) {
+            } else if (dividend == divisor) {
                 result.push_back(1);
-                continue;
+            } else {
+                visited.clear();
+                result.push_back(dfs(visited, dividend, divisor, 1.0, graph));
             }
-            visited.clear();
-            result.push_back(compute(visited, 1, numerator, denominator, graph));
         }
         return result;
     }
 
 private:
-    // graph[i][j] = result of i/j
-    using BiGraph = std::unordered_map<std::string, std::unordered_map<std::string, double>>;
+    using Graph = std::unordered_map<std::string, std::unordered_map<std::string, double>>;
 
-    double compute(std::unordered_set<std::string>& visited, double product,
-                   const std::string& current, const std::string& target, const BiGraph& graph)
+    double dfs(std::unordered_set<std::string>& visited, const std::string& start,
+               const std::string& target, double product, const Graph& graph)
     {
-        if (current == target)
+        if (start == target)
             return product;
 
+        visited.insert(start);
         double result = -1;
-        visited.insert(current);
-        for (const auto& [adj, val] : graph.at(current)) {
+        for (const auto& [adj, val] : graph.at(start)) {
             if (visited.count(adj))
                 continue;
 
-            result = compute(visited, product * val, adj, target, graph);
+            result = dfs(visited, adj, target, product * val, graph);
             if (result != -1)
                 break;
         }
-        visited.erase(current);
         return result;
     }
 };
