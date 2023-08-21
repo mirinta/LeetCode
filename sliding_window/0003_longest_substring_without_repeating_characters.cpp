43b5ba50b6bae89a95cc0ca63f5f556b616ac459
mirinta <mirinta@163.com>
2023-08-22 00:57:19 +0800

    solutions

--- sliding_window/0003_longest_substring_without_repeating_characters.cpp
+++ sliding_window/0003_longest_substring_without_repeating_characters.cpp
@@ -4,34 +4,50 @@
 /**
  * Given a string "s", find the length of the longest substring without repeating characters.
  *
- * Example 1:
- * Input: s = "abcabcbb"
- * Output: 3, the answer if "abc", with the length of 3
- *
- * Example 2:
- * Input: s = "bbbbb"
- * Output: 1, the answer is "b", with the length of 1
- *
- * Example 3:
- * Input: s = "pwwkew"
- * Output: 3, the answer is "wke", with the length of 3
+ * ! 0 <= s.length <= 5 * 10^4
+ * ! s consists of English letters, digits, symbols and spaces.
  */
 
 class Solution
 {
 public:
-    int lengthOfLongestSubstring(std::string s)
+    int lengthOfLongestSubstring(std::string s) { return approach2(s); }
+
+private:
+    // time O(N), space O(N)
+    int approach2(const std::string& s)
     {
+        if (s.size() < 2)
+            return s.size();
+
         int result = 0;
-        std::unordered_map<char, int> freq;
-        // sliding window, elements in range [left, right)
+        std::unordered_map<char, int> map; // letter to index
+        for (int i = 0, j = 0; i < s.size(); ++i) {
+            if (map.count(s[i])) {
+                j = std::max(j, map[s[i]] + 1);
+            }
+            map[s[i]] = i;
+            result = std::max(result, i - j + 1);
+        }
+        return result;
+    }
+
+    // time O(N), space O(N)
+    int approach1(const std::string& s)
+    {
+        if (s.size() < 2)
+            return s.size();
+
+        int result = 0;
         int left = 0;
         int right = 0;
+        std::unordered_map<char, int> map; // letter to frequency
+        // sliding window, elements in range [left, right)
         while (right < s.size()) {
-            const char letter = s[right++];
-            freq[letter]++;
-            while (freq[letter] > 1) {
-                freq[s[left++]]--;
+            const auto c = s[right++];
+            map[c]++;
+            while (map[c] > 1) {
+                map[s[left++]]--;
             }
             result = std::max(result, right - left);
         }
