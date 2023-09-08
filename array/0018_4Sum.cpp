64aa6a393da518ac6fe7bfd8e2bee74d619ed2ab
mirinta <mirinta@163.com>
2023-09-08 12:03:00 +0800

    Update 0018_4Sum.cpp

--- array/0018_4Sum.cpp
+++ array/0018_4Sum.cpp
@@ -12,6 +12,10 @@
  * - nums[a] + nums[b] + nums[c] + nums[d] = target
  *
  * You may return the answer in any order.
+ *
+ * ! 1 <= nums.length <= 200
+ * ! -10^9 <= nums[i] <= 10^9
+ * ! -10^9 <= target <= 10^9
  */
 
 class Solution
@@ -19,51 +23,57 @@ class Solution
 public:
     std::vector<std::vector<int>> fourSum(std::vector<int>& nums, int target)
     {
+        if (nums.size() < 4)
+            return {};
+
         std::sort(nums.begin(), nums.end());
-        return nSum(nums, target, 4, 0);
+        return nSum(0, target, 4, nums);
     }
 
 private:
-    // the input array should be sorted in ascending order
-    std::vector<std::vector<int>> nSum(std::vector<int>& nums, long long target, int n, int start)
+    std::vector<std::vector<int>> nSum(int start, long long target, int n,
+                                       const std::vector<int>& nums)
     {
-        if (nums.empty() || nums.size() < n)
+        if (nums.size() - start < n)
             return {};
 
         std::vector<std::vector<int>> result;
         if (n == 2) {
-            int low = start;
-            int high = nums.size() - 1;
-            while (low < high) {
-                const auto& valLeft = nums[low];
-                const auto& valRight = nums[high];
-                const long long sum = valLeft + valRight;
+            int left = start;
+            int right = nums.size() - 1;
+            while (left < right) {
+                const int leftVal = nums[left];
+                const int rightVal = nums[right];
+                const long long sum = leftVal + rightVal;
                 if (sum > target) {
-                    while (low < high && nums[high] == valRight) {
-                        high--;
+                    while (left < right && nums[right] == rightVal) {
+                        right--;
                     }
                 } else if (sum < target) {
-                    while (low < high && nums[low] == valLeft) {
-                        low++;
+                    while (left < right && nums[left] == leftVal) {
+                        left++;
                     }
                 } else {
-                    result.push_back({valLeft, valRight});
-                    while (low < high && nums[low] == valLeft) {
-                        low++;
+                    result.push_back({leftVal, rightVal});
+                    while (left < right && nums[right] == rightVal) {
+                        right--;
                     }
-                    while (low < high && nums[high] == valRight) {
-                        high--;
+                    while (left < right && nums[left] == leftVal) {
+                        left++;
                     }
                 }
             }
         } else {
             for (int i = start; i < nums.size(); ++i) {
-                auto subResults = nSum(nums, target - nums[i], n - 1, i + 1);
+                auto subResults = nSum(i + 1, target - nums[i], n - 1, nums);
+                if (subResults.empty())
+                    continue;
+
                 for (auto& sub : subResults) {
                     sub.push_back(nums[i]);
                     result.push_back(std::move(sub));
                 }
-                while (i < nums.size() - 1 && nums[i] == nums[i + 1]) {
+                while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
                     i++;
                 }
             }
