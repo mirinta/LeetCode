3be991a62ee9ddede553d6fbf50b9f6bc3b78678
mirinta <mirinta@163.com>
2023-09-11 00:34:23 +0800

    refine

--- binary_tree/0094_binary_tree_inorder_traversal.cpp
+++ binary_tree/0094_binary_tree_inorder_traversal.cpp
@@ -1,3 +1,4 @@
+#include <stack>
 #include <vector>
 
 /**
@@ -15,22 +16,45 @@ struct TreeNode
 
 /**
  * Given the "root" of a binary tree, return the inorder traversal of its nodes' values.
+ *
+ * ! The number of nodes in the tree is in the range [0, 100].
+ * ! -100 <= Node.val <= 100
  */
 
 class Solution
 {
 public:
-    std::vector<int> inorderTraversal(TreeNode* root)
+    std::vector<int> inorderTraversal(TreeNode* root) { return approach2(root); }
+
+private:
+    std::vector<int> approach2(TreeNode* root)
     {
         if (!root)
             return {};
 
         std::vector<int> result;
+        std::stack<TreeNode*> stack;
+        auto* node = root;
+        while (node || !stack.empty()) {
+            while (node) {
+                stack.push(node);
+                node = node->left;
+            }
+            auto* top = stack.top();
+            stack.pop();
+            result.push_back(top->val);
+            node = top->right;
+        }
+        return result;
+    }
+
+    std::vector<int> approach1(TreeNode* root)
+    {
+        std::vector<int> result;
         traverse(result, root);
         return result;
     }
 
-private:
     void traverse(std::vector<int>& result, TreeNode* node)
     {
         if (!node)
