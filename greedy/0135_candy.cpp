7261335d82aaea2c3484ab355fd3037869a6a549
mirinta <mirinta@163.com>
2023-09-13 11:02:19 +0800

    Update 0135_candy.cpp

--- greedy/0135_candy.cpp
+++ greedy/0135_candy.cpp
@@ -25,14 +25,24 @@ public:
     {
         const int n = ratings.size();
         std::vector<int> candies(n, 1);
+        // step1: assign candies to the ith child according to his left neighbour
         for (int i = 1; i < n; ++i) {
             if (ratings[i] > ratings[i - 1]) {
-                candies[i] = 1 + candies[i - 1];
+                candies[i] = candies[i - 1] + 1;
             }
         }
+        // step2: assign candies to the ith child according to his right neighbour
+        // why not iterate forward?
+        // assume:
+        // indices: 0 1 2
+        // ratings: 2 2 1
+        // after step1: candies = [1,1,1]
+        // if we iterate forward and check the right neighbour of the ith child:
+        // candies = [1,2,1], because the condition is satisfied when we are at ratings[1]
+        // ratings[0] is the same as ratings[1], but it is not updated
         for (int i = n - 2; i >= 0; --i) {
             if (ratings[i] > ratings[i + 1]) {
-                candies[i] = std::max(candies[i], 1 + candies[i + 1]);
+                candies[i] = std::max(candies[i], candies[i + 1] + 1);
             }
         }
         return std::accumulate(candies.begin(), candies.end(), 0);
