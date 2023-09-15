7d506954bcf0fd0e4b3164c57924e4ca6657db6c
mirinta <mirinta@163.com>
2023-09-15 15:33:13 +0800

    Update 0049_group_anagrams.cpp

--- string/0049_group_anagrams.cpp
+++ string/0049_group_anagrams.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <array>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -24,44 +25,50 @@ public:
     }
 
 private:
+    // time O(NK), space O(NK)
+    // - N = strs.size(), K = maximum length of strs[i]
     std::vector<std::vector<std::string>> approach2(const std::vector<std::string>& strs)
     {
-        std::vector<int> freq(26, 0);
-        auto encode = [&freq](const std::string& word) {
-            std::fill(freq.begin(), freq.end(), 0);
-            for (const auto& c : word) {
-                freq[c - 'a']++;
-            }
-            std::string result;
-            for (int i = 0; i < 26; ++i) {
-                if (freq[i] == 0)
-                    continue;
-
-                result.push_back(i + 'a');
-                result.push_back(freq[i]);
-            }
-            return result;
-        };
-        std::unordered_map<std::string, std::vector<std::string>> map;
-        for (const auto& word : strs) {
-            map[encode(word)].push_back(word);
-        }
         std::vector<std::vector<std::string>> result;
+        std::unordered_map<std::string, std::vector<std::string>> map;
+        for (const auto& s : strs) {
+            map[encode(s)].push_back(s);
+        }
         for (auto& [key, anagrams] : map) {
             result.push_back(std::move(anagrams));
         }
         return result;
     }
 
+    std::string encode(const std::string& s)
+    {
+        // counting sort
+        constexpr int R = 26;
+        std::array<int, R> count{0};
+        for (const auto& c : s) {
+            count[c - 'a']++;
+        }
+        std::string result;
+        for (int i = 0; i < R; ++i) {
+            if (count[i] > 0) {
+                result.push_back(i + 'a');
+                result.push_back(count[i]);
+            }
+        }
+        return result;
+    }
+
+    // time O(NKlogK), space O(NK)
+    // - N = strs.size(), K = maximum length of strs[i]
     std::vector<std::vector<std::string>> approach1(const std::vector<std::string>& strs)
     {
-        std::unordered_map<std::string, std::vector<std::string>> map;
-        for (const auto& word : strs) {
-            std::string key(word);
-            std::sort(key.begin(), key.end());
-            map[key].push_back(word);
-        }
         std::vector<std::vector<std::string>> result;
+        std::unordered_map<std::string, std::vector<std::string>> map;
+        for (const auto& s : strs) {
+            std::string key(s);
+            std::sort(key.begin(), key.end());
+            map[key].push_back(s);
+        }
         for (auto& [key, anagrams] : map) {
             result.push_back(std::move(anagrams));
         }
