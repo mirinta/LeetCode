3331198c2adbd7175bd483b2473ae14a451322ce
mirinta <mirinta@163.com>
2023-09-20 14:57:45 +0800

    Update 0056_merge_intervals.cpp

--- interval_scheduling/0056_merge_intervals.cpp
+++ interval_scheduling/0056_merge_intervals.cpp
@@ -14,8 +14,45 @@
 class Solution
 {
 public:
-    // time O(NlogN), space O(std::sort+N)
     std::vector<std::vector<int>> merge(std::vector<std::vector<int>>& intervals)
+    {
+        return approach1(intervals);
+    }
+
+private:
+    // sweeping line, time O(NlogN), space O(N)
+    std::vector<std::vector<int>> approach2(const std::vector<std::vector<int>>& intervals)
+    {
+        std::vector<std::pair<int, int>>
+            points; // <coord,flag>, flag=1(starting point),-1(ending point)
+        points.reserve(intervals.size() * 2);
+        for (const auto& interval : intervals) {
+            points.emplace_back(interval[0], 1);
+            points.emplace_back(interval[1], -1);
+        }
+        std::sort(points.begin(), points.end(), [](const auto& p1, const auto& p2) {
+            return p1.first == p2.first ? p1.second > p2.second : p1.first < p2.first;
+        });
+        int startingPoint = 0;
+        int endingPoint = 0;
+        int count = 0;
+        std::vector<std::vector<int>> result;
+        for (const auto& [coord, flag] : points) {
+            if (count == 0 && count + flag > 0) {
+                // this point starts a new interval
+                startingPoint = coord;
+            } else if (count > 0 && count + flag == 0) {
+                // this point is the end of the current interval
+                endingPoint = coord;
+                result.push_back({startingPoint, endingPoint});
+            }
+            count += flag;
+        }
+        return result;
+    }
+
+    // sorting, time O(NlogN), space O(N)
+    std::vector<std::vector<int>> approach1(std::vector<std::vector<int>>& intervals)
     {
         if (intervals.empty())
             return {};
