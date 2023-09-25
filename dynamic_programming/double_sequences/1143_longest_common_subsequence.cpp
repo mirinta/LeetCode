7d51c164c5f19c28298f381a737b0aad6c8dd2cd
mirinta <mirinta@163.com>
2023-09-25 16:42:40 +0800

    Update 1143_longest_common_subsequence.cpp

--- dynamic_programming/double_sequences/1143_longest_common_subsequence.cpp
+++ dynamic_programming/double_sequences/1143_longest_common_subsequence.cpp
@@ -1,13 +1,34 @@
-class Solution {
+#include <string>
+#include <vector>
+
+/**
+ * Given two strings "text1" and "text2", return the length of their longest common subsequence. If
+ * there is no common subsequence, return 0.
+ *
+ * A subsequence of a string is a new string generated from the original string with some characters
+ * (can be none) deleted without changing the relative order of the remaining characters.
+ *
+ * - For example, "ace" is a subsequence of "abcde".
+ *
+ * A common subsequence of two strings is a subsequence that is common to both strings.
+ *
+ * ! 1 <= text1.length, text2.length <= 1000
+ * ! text1 and text2 consist of only lowercase English characters.
+ */
+
+class Solution
+{
 public:
-    int longestCommonSubsequence(string text1, string text2) {
+    int longestCommonSubsequence(std::string text1, std::string text2)
+    {
         return approach2(text1, text2);
     }
 
 private:
     // DP with space optimization, time O(MN), space O(N)
     // - M and N are the length of s1 and s2, respectively
-    int approach2(const std::string& s1, const std::string& s2) {
+    int approach2(const std::string& s1, const std::string& s2)
+    {
         const int m = s1.size();
         const int n = s2.size();
         std::vector<int> dp(n + 1, 0);
@@ -26,7 +47,8 @@ private:
 
     // DP, time O(MN), space O(MN)
     // - M and N are the length of s1 and s2, respectively
-    int approach1(const std::string& s1, const std::string& s2) {
+    int approach1(const std::string& s1, const std::string& s2)
+    {
         const int m = s1.size();
         const int n = s2.size();
         // dp[i][j] = length of LCS of s1[0:i) and s2[0:j)
