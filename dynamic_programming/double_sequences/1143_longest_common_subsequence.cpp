d79057cbf091479c16e2a4cd78d87b2d1cb4e239
mirinta <mirinta@163.com>
2023-09-25 16:42:20 +0800

    Update 1143_longest_common_subsequence.cpp

--- dynamic_programming/double_sequences/1143_longest_common_subsequence.cpp
+++ dynamic_programming/double_sequences/1143_longest_common_subsequence.cpp
@@ -1,59 +1,40 @@
-#include <string>
-#include <vector>
-
-/**
- * Given two strings "text1" and "text2", return the length of their longest common subsequence. If
- * there is no common subsequence, return 0.
- *
- * A subsequence of a string is a new string generated from the original string with some characters
- * (can be none) deleted without changing the relative order of the remaining characters.
- *
- * - For example, "ace" is a subsequence of "abcde".
- *
- * A common subsequence of two strings is a subsequence that is common to both strings.
- */
-
-class Solution
-{
+class Solution {
 public:
-    int longestCommonSubsequence(std::string text1, std::string text2)
-    {
+    int longestCommonSubsequence(string text1, string text2) {
         return approach2(text1, text2);
     }
 
 private:
-    int approach2(const std::string& text1, const std::string& text2)
-    {
-        // DP with space optimization
-        const int m = text1.size();
-        const int n = text2.size();
-        std::vector<int> iMinus1(n + 1, 0);
+    // DP with space optimization, time O(MN), space O(N)
+    // - M and N are the length of s1 and s2, respectively
+    int approach2(const std::string& s1, const std::string& s2) {
+        const int m = s1.size();
+        const int n = s2.size();
         std::vector<int> dp(n + 1, 0);
         for (int i = 1; i <= m; ++i) {
+            auto prev = dp;
             for (int j = 1; j <= n; ++j) {
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[j] = iMinus1[j - 1] + 1;
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[j] = 1 + prev[j - 1];
                 } else {
-                    dp[j] = std::max(iMinus1[j], dp[j - 1]);
+                    dp[j] = std::max(prev[j], dp[j - 1]);
                 }
             }
-            iMinus1 = dp;
         }
         return dp[n];
     }
 
-    int approach1(const std::string& text1, const std::string& text2)
-    {
-        const int m = text1.size();
-        const int n = text2.size();
-        // dp[i][j] = length of LCS between text1[0:i) and text2[0:j)
+    // DP, time O(MN), space O(MN)
+    // - M and N are the length of s1 and s2, respectively
+    int approach1(const std::string& s1, const std::string& s2) {
+        const int m = s1.size();
+        const int n = s2.size();
+        // dp[i][j] = length of LCS of s1[0:i) and s2[0:j)
         std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
-                // X X [?]
-                // Y [?]
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
                     dp[i][j] = std::max(dp[i - 1][j], dp[i][j - 1]);
                 }
