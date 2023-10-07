20103ee216a6565e6c0f1b324cac36561a282181
mirinta <mirinta@163.com>
2023-10-07 14:59:03 +0800

    Update 0003_longest_substring_without_repeating_characters.cpp

--- sliding_window/0003_longest_substring_without_repeating_characters.cpp
+++ sliding_window/0003_longest_substring_without_repeating_characters.cpp
@@ -11,45 +11,47 @@
 class Solution
 {
 public:
-    int lengthOfLongestSubstring(std::string s) { return approach2(s); }
+    int lengthOfLongestSubstring(std::string s) { return approach1(s); }
 
 private:
     // time O(N), space O(N)
     int approach2(const std::string& s)
     {
-        if (s.size() < 2)
-            return s.size();
+        const int n = s.size();
+        if (n <= 1)
+            return n;
 
         int result = 0;
-        std::unordered_map<char, int> map; // letter to index
-        for (int i = 0, j = 0; i < s.size(); ++i) {
+        std::unordered_map<char, int> map; // letter to its last occurrence index
+        for (int i = 0, j = 0; i < n; ++i) {
+            // ... j j+1 ... i ...
+            // if s[i] == s[j], then s[j+1:i] is what we want
             if (map.count(s[i])) {
                 j = std::max(j, map[s[i]] + 1);
             }
-            map[s[i]] = i;
             result = std::max(result, i - j + 1);
+            map[s[i]] = i;
         }
         return result;
     }
 
-    // time O(N), space O(N)
+    // sliding window, time O(N), space O(N)
     int approach1(const std::string& s)
     {
-        if (s.size() < 2)
-            return s.size();
+        const int n = s.size();
+        if (n <= 1)
+            return n;
 
         int result = 0;
-        int left = 0;
-        int right = 0;
         std::unordered_map<char, int> map; // letter to frequency
-        // sliding window, elements in range [left, right)
-        while (right < s.size()) {
-            const auto c = s[right++];
-            map[c]++;
-            while (map[c] > 1) {
-                map[s[left++]]--;
+        for (int left = 0, right = 0; right < n; ++right) {
+            const auto letter = s[right];
+            map[letter]++;
+            while (map[letter] > 1) {
+                map[s[left]]--;
+                left++;
             }
-            result = std::max(result, right - left);
+            result = std::max(result, right - left + 1);
         }
         return result;
     }
