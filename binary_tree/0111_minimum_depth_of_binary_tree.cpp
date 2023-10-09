b4cbfba4db3c00f247482c1aa767f2ef26f2d51e
mirinta <mirinta@163.com>
2023-10-09 11:58:17 +0800

    Update 0111_minimum_depth_of_binary_tree.cpp

--- binary_tree/0111_minimum_depth_of_binary_tree.cpp
+++ binary_tree/0111_minimum_depth_of_binary_tree.cpp
@@ -18,23 +18,47 @@ struct TreeNode
  *
  * The minimum depth is the number of nodes along the shortest path from the root node down to the
  * nearest leaf node.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * ! The number of nodes in the tree is in the range [0, 10^5].
+ * ! -1000 <= Node.val <= 1000
  */
 
 class Solution
 {
 public:
-    int minDepth(TreeNode* root)
+    int minDepth(TreeNode* root) { return approach2(root); }
+
+private:
+    // DFS, time O(N), space O(N)
+    int approach2(TreeNode* root)
     {
         if (!root)
             return 0;
 
-        // BFS: level traverse
-        int result = 1;
+        if (!root->left)
+            return 1 + approach2(root->right);
+
+        if (!root->right)
+            return 1 + approach2(root->left);
+
+        return 1 + std::min(approach2(root->left), approach2(root->right));
+    }
+
+    // BFS, time O(N), space O(N)
+    int approach1(TreeNode* root)
+    {
+        if (!root)
+            return 0;
+
+        // level-order traverse, once we reach a leaf node,
+        // its depth is the final answer
+        int result = 1; // we count the num of nodes along the path
         std::queue<TreeNode*> queue;
         queue.push(root);
         while (!queue.empty()) {
-            const auto size = queue.size();
-            for (size_t i = 0; i < size; ++i) {
+            for (int size = queue.size(); size > 0; --size) {
                 auto* node = queue.front();
                 queue.pop();
                 if (!node->left && !node->right)
@@ -49,6 +73,6 @@ public:
             }
             result++;
         }
-        return result;
+        return -1;
     }
 };
