a1fc76d44cfca5728e5d64f34a99986e676a0e91
mirinta <mirinta@163.com>
2023-10-12 16:35:01 +0800

    Update 0513_find_bottom_left_tree_value.cpp

--- binary_tree/0513_find_bottom_left_tree_value.cpp
+++ binary_tree/0513_find_bottom_left_tree_value.cpp
@@ -14,30 +14,31 @@ struct TreeNode
 };
 
 /**
- * Given the "root" of a binary tree, return the leftmost value in the last row of the tree.
+ * Given the root of a binary tree, return the leftmost value in the last row of the tree.
+ *
+ * ! The number of nodes in the tree is in the range [1, 10^4].
+ * ! -2^31 <= Node.val <= 2^31 - 1
  */
 
 class Solution
 {
+public:
     int findBottomLeftValue(TreeNode* root)
     {
-        if (!root)
-            return -1;
-
+        int result = 0;
         std::queue<TreeNode*> queue;
         queue.push(root);
-        int result = -1;
         while (!queue.empty()) {
-            auto* node = queue.front();
-            queue.pop();
-            result = node->val;
-            // add right node first, then add left node
-            // to make sure the leftmost node of each level is at the end of the queue
-            if (node->right) {
-                queue.push(node->right);
-            }
-            if (node->left) {
-                queue.push(node->left);
+            result = queue.front()->val;
+            for (int i = queue.size(); i > 0; --i) {
+                auto* node = queue.front();
+                queue.pop();
+                if (node->left) {
+                    queue.push(node->left);
+                }
+                if (node->right) {
+                    queue.push(node->right);
+                }
             }
         }
         return result;
