4ba66e22110e7b4d0ce1ef3b31cd6f87c70c3958
mirinta <mirinta@163.com>
2023-10-16 16:53:46 +0800

    Update 0518_coin_change_II.cpp

--- dynamic_programming/knapsack/0518_coin_change_II.cpp
+++ dynamic_programming/knapsack/0518_coin_change_II.cpp
@@ -23,47 +23,45 @@ public:
     int change(int amount, std::vector<int>& coins) { return approach2(amount, coins); }
 
 private:
-    // 1D-DP, time O(AN), space O(A)
-    // A is the amount, and N is the number of coins
+    // DP with space optimization
     int approach2(int amount, const std::vector<int>& coins)
     {
         const int n = coins.size();
         std::vector<int> dp(amount + 1, 0);
-        dp[0] = 1;
         for (int i = 1; i <= n; ++i) {
+            dp[0] = 1;
             for (int j = 1; j <= amount; ++j) {
-                if (j >= coins[i - 1]) {
-                    dp[j] += dp[j - coins[i - 1]];
+                if (j - coins[i - 1] >= 0) {
+                    dp[j] = dp[j] + dp[j - coins[i - 1]];
                 }
             }
         }
         return dp[amount];
     }
 
-    // 2D-DP, time O(AN), space O(AN)
-    // A is the amount, and N is the number of coins
+    // DP
     int approach1(int amount, const std::vector<int>& coins)
     {
-        const int n = coins.size();
-        // dp[i][j] = num of combinations to make up amount j using coins[0:i)
-        std::vector<std::vector<int>> dp(n + 1, std::vector<int>(amount + 1, 0));
+        // dp[i][j] = num of ways to make up amount j using coins[0:i)
         // base cases:
-        // - dp[0][0] = 1, only one way to make up zero amount with empty coin list: choose nothing
-        // - dp[i][0] = 1, same as above
-        // - dp[0][j] = 0, no way to make up non-zero amount with an empty coin list
+        // - dp[0][0] = 1, only one way to make up amount 0 with an empty coin list: do nothing
+        // - dp[i>0][0] = 1, only one way to make up amount 0 with a non-empty coin list
+        // - dp[0][j>0] = 0, no way to make up j>0 with an empty coin list
+        const int n = coins.size();
+        std::vector<std::vector<int>> dp(n + 1, std::vector<int>(amount + 1, 0));
         for (int i = 0; i <= n; ++i) {
             dp[i][0] = 1;
         }
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= amount; ++j) {
-                // case1: the ith coin is not used
-                dp[i][j] = dp[i - 1][j];
-                // case2: the ith coin is used
-                if (j >= coins[i - 1]) {
-                    // not dp[i][j] += dp[i-1][j-coins[i-1]],
-                    // because the number of each coin is infinite,
-                    // we can still use the ith coin to make up the remaining amount
-                    dp[i][j] += dp[i][j - coins[i - 1]];
+                // option1: the ith coin is not used, then we make up amount j using coins[0,i-1)
+                // option2: the ith coin is used, then we make up amount j-coins[i-1] using
+                // coins[0:i)
+                // - it is still coins[0:i) because the number of each coin is infinite
+                if (j - coins[i - 1] < 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
                 }
             }
         }
