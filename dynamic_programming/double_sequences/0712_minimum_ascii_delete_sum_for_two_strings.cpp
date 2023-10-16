d69415280c468a85d8725ac26cc800956ac0f674
mirinta <mirinta@163.com>
2023-10-16 18:08:04 +0800

    Update 0712_minimum_ascii_delete_sum_for_two_strings.cpp

--- dynamic_programming/double_sequences/0712_minimum_ascii_delete_sum_for_two_strings.cpp
+++ dynamic_programming/double_sequences/0712_minimum_ascii_delete_sum_for_two_strings.cpp
@@ -2,31 +2,57 @@
 #include <vector>
 
 /**
- * Given two strings "s1" and "s2", return the lowest ASCII sum of deleted characters to make two
+ * Given two strings s1 and s2, return the lowest ASCII sum of deleted characters to make two
  * strings equal.
  *
- * Example:
- * Input: s1 = "sea", s2 = "eat"
- * Output: 231
- * Explanation: Deleting "s" from "sea" adds the ASCII value of "s" (115) to the sum. Deleting "t"
- * from "eat" adds 116 to the sum. At the end, both strings are equal, and 115 + 116 = 231 is the
- * minimum sum possible to achieve this.
+ * ! 1 <= s1.length, s2.length <= 1000
+ * ! s1 and s2 consist of lowercase English letters.
  */
 
 class Solution
 {
 public:
-    int minimumDeleteSum(std::string s1, std::string s2)
+    int minimumDeleteSum(const std::string& s1, const std::string& s2) { return approach2(s1, s2); }
+
+private:
+    // DP with space optimization
+    int approach2(const std::string& s1, const std::string& s2)
     {
-        // similar to LC 1143
-        const auto m = s1.size();
-        const auto n = s2.size();
-        // dp[i][j] = minDeleteSum of s1[0:i) and s2[0:j)
-        std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
+        const int m = s1.size();
+        const int n = s2.size();
+        std::vector<int> dp(1 + n, 0);
+        for (int j = 1; j <= n; ++j) {
+            dp[j] = dp[j - 1] + s2[j - 1];
+        }
+        auto prev = dp;
+        for (int i = 1; i <= m; ++i) {
+            prev = dp;
+            dp[0] = prev[0] + s1[i - 1];
+            for (int j = 1; j <= n; ++j) {
+                const auto& c1 = s1[i - 1];
+                const auto& c2 = s2[j - 1];
+                if (c1 == c2) {
+                    dp[j] = prev[j - 1];
+                } else {
+                    dp[j] = std::min(c1 + prev[j], c2 + dp[j - 1]);
+                }
+            }
+        }
+        return dp[n];
+    }
+
+    // DP
+    int approach1(const std::string& s1, const std::string& s2)
+    {
+        const int m = s1.size();
+        const int n = s2.size();
+        // dp[i][j] = min ASCII sum of deleted characters to make s1[0:i) and s2[0:j) the same
         // base cases:
-        // - dp[i][0], s2[0:0) is empty, all characters of s1[0:i) need to be deleted
-        // - dp[0][j], s1[0:0) is empty, all characters of s2[0:j) need to be deleted
-        // - dp[0][0], both s1[0:0) and s2[0:0) are empty, no deletion
+        // - dp[0][0] = 0
+        // - dp[i>0][0] = ASCII sum of s1[0:i), i.e., delete all characters of s1[0:i)
+        // - dp[0][j>0] = ASCII sum of s2[0:j), i.e., delete all characters of s2[0:j)
+        std::vector<std::vector<int>> dp(1 + m, std::vector<int>(1 + n, INT_MAX));
+        dp[0][0] = 0;
         for (int i = 1; i <= m; ++i) {
             dp[i][0] = dp[i - 1][0] + s1[i - 1];
         }
@@ -37,11 +63,10 @@ public:
             for (int j = 1; j <= n; ++j) {
                 const auto& c1 = s1[i - 1];
                 const auto& c2 = s2[j - 1];
-                // if c1 = c2 = c, it means c is part of the LCS, no deletion
-                // otherwise, either c1 or c2 is not part of the LCS, delete one of them
                 if (c1 == c2) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dp[i][j] = dp[i - 1][j - 1]; // both c1 and c2 are not deleted
                 } else {
+                    // delete either c1 or c2
                     dp[i][j] = std::min(c1 + dp[i - 1][j], c2 + dp[i][j - 1]);
                 }
             }
