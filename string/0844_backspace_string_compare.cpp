f90d26b7c146cc0652d1348be9d08ecea7549373
mirinta <mirinta@163.com>
2023-10-19 11:30:50 +0800

    Update 0844_backspace_string_compare.cpp

--- string/0844_backspace_string_compare.cpp
+++ string/0844_backspace_string_compare.cpp
@@ -2,64 +2,83 @@
 #include <string>
 
 /**
- * Given two strings "s" and "t", return "true" if they are equal when both are typed into empty
- * text editors. "#" means a backspace character.
+ * Given two strings s and t, return true if they are equal when both are typed into empty text
+ * editors. '#' means a backspace character.
  *
  * Note that after backspacing an empty text, the text will continue empty.
+ *
+ * ! 1 <= s.length, t.length <= 200
+ * ! s and t only contain lowercase letters and '#' characters.
  */
 
 class Solution
 {
 public:
-    bool backspaceCompare(const std::string& s, const std::string& t)
-    {
-        // approach 1: two stacks:
-        std::stack<char> stackS;
-        process(stackS, s);
-        std::stack<char> stackT;
-        process(stackT, t);
-        if (stackS.size() != stackT.size())
-            return false;
+    bool backspaceCompare(const std::string& s, const std::string& t) { return approach2(s, t); }
 
-        for (; !stackS.empty() && !stackT.empty(); stackS.pop(), stackT.pop()) {
-            if (stackS.top() != stackT.top())
+private:
+    // two-pointers, time O(N), space O(1)
+    bool approach2(const std::string& s, const std::string& t)
+    {
+        int idxS = s.size() - 1;
+        int idxT = t.size() - 1;
+        int countS = 0;
+        int countT = 0;
+        while (true) {
+            while (idxS >= 0 && (s[idxS] == '#' || countS > 0)) {
+                if (s[idxS--] == '#') {
+                    countS++;
+                } else {
+                    countS--;
+                }
+            }
+            while (idxT >= 0 && (t[idxT] == '#' || countT > 0)) {
+                if (t[idxT--] == '#') {
+                    countT++;
+                } else {
+                    countT--;
+                }
+            }
+            if (idxS < 0 && idxT < 0)
+                break;
+
+            if (idxS < 0 || idxT < 0)
+                return false;
+
+            if (s[idxS--] != t[idxT--])
                 return false;
         }
         return true;
-        // approach 2: two pointers
-        // const char backspace = '#';
-        // int idxS = s.size() - 1;
-        // int countS = 0;
-        // int idxT = t.size() - 1;
-        // int countT = 0;
-        // while (true) {
-        //     while (idxS >= 0 && (s[idxS] == backspace || countS > 0)) {
-        //         s[idxS--] == backspace ? countS++ : countS--;
-        //     }
-        //     while (idxT >= 0 && (t[idxT] == backspace || countT > 0)) {
-        //         t[idxT--] == backspace ? countT++ : countT--;
-        //     }
-        //     if (idxS < 0 || idxT < 0)
-        //         break;
-
-        //     if (s[idxS--] != t[idxT--])
-        //         return false;
-        // }
-        // return idxS == idxT;
     }
 
-private:
-    void process(std::stack<char>& stack, const std::string& s)
+    // two stacks, time O(N), space O(N)
+    bool approach1(const std::string& s, const std::string& t)
     {
-        const char backspace = '#';
+        auto stack1 = genStack(s);
+        auto stack2 = genStack(t);
+        if (stack1.size() != stack2.size())
+            return false;
+
+        while (!stack1.empty() && !stack2.empty()) {
+            if (stack1.top() != stack2.top())
+                return false;
+
+            stack1.pop();
+            stack2.pop();
+        }
+        return true;
+    }
+
+    std::stack<char> genStack(const std::string& s)
+    {
+        std::stack<char> stack;
         for (const auto& c : s) {
-            if (c == backspace) {
-                if (!stack.empty()) {
-                    stack.pop();
-                }
-            } else {
+            if (c != '#') {
                 stack.push(c);
+            } else if (!stack.empty()) {
+                stack.pop();
             }
         }
+        return stack;
     }
 };
