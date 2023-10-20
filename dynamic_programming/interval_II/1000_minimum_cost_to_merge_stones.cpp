fdb2a48006e09a58a006fbb79c7afeffa790bf7f
mirinta <mirinta@163.com>
2023-10-20 13:12:57 +0800

    Update 1000_minimum_cost_to_merge_stones.cpp

--- dynamic_programming/interval_II/1000_minimum_cost_to_merge_stones.cpp
+++ dynamic_programming/interval_II/1000_minimum_cost_to_merge_stones.cpp
@@ -20,28 +20,26 @@ class Solution
 public:
     int mergeStones(std::vector<int>& stones, int k)
     {
-        // each time we merge k piles into 1 pile,
-        // the total num of piles decreases by k-1
-        // assume we have N piles at the beginning,
-        // and we finally can merge them into 1 pile
-        // then, the total decrement is N-1
-        // thus, the problem is solvable if and only if (N-1) % (k-1) == 0
+        // Each time we merge k piles into 1 pile,
+        // the number of piles decreases by k-1.
+        // Assume we have N piles at the beginning.
+        // If we can finally merge them into 1 pile,
+        // the total decrement is N-1.
+        // Thus, the problem is solvable if and only if (N-1)%(k-1) equals 0.
         const int n = stones.size();
         if ((n - 1) % (k - 1) != 0)
             return -1;
 
-        std::vector<int> sum(1 + n, 0); // sum[i] = sum of stones[0:i-1]
+        std::vector<int> prefixSum(1 + n, 0);
         for (int i = 1; i <= n; ++i) {
-            sum[i] = sum[i - 1] + stones[i - 1];
+            prefixSum[i] = prefixSum[i - 1] + stones[i - 1];
         }
+        // To get the final 1 pile, we need to merge N piles into k piles first.
         // dp[i][j][p] = min cost to merge stones[i:j] into p piles
-        // i X X X X X X X X X X q q+1 X X X X X X X X X X X j
-        // |<-merge into 1 pile->| |<--merge into p-1 piles->|
-        // |<----dp[i][q][1]---->| |<----dp[q+1][j][p-1]---->|
-        // |<---------------merge into p piles-------------->|
-        // then, merge these p piles into 1 pile
+        // i ................ x x+1 ................. j
+        // |<-merge to 1 pile>| |<-merge to p-1 pile->|
         std::vector<std::vector<std::vector<int>>> dp(
-            n, std::vector<std::vector<int>>(n, std::vector<int>(1 + k, INT_MAX)));
+            n, std::vector<std::vector<int>>(n, std::vector<int>(k + 1, INT_MAX)));
         for (int i = 0; i < n; ++i) {
             dp[i][i][1] = 0;
         }
@@ -52,16 +50,17 @@ public:
                     if (p > length)
                         continue;
 
-                    for (int q = i; q < j; ++q) {
-                        if (dp[i][q][1] == INT_MAX || dp[q + 1][j][p - 1] == INT_MAX)
+                    for (int x = i; x < j; ++x) {
+                        if (dp[i][x][1] == INT_MAX || dp[x + 1][j][p - 1] == INT_MAX)
                             continue;
 
-                        dp[i][j][p] = std::min(dp[i][j][p], dp[i][q][1] + dp[q + 1][j][p - 1]);
+                        dp[i][j][p] = std::min(dp[i][j][p], dp[i][x][1] + dp[x + 1][j][p - 1]);
                     }
                 }
-                if (dp[i][j][k] != INT_MAX) {
-                    dp[i][j][1] = dp[i][j][k] + sum[j + 1] - sum[i];
-                }
+                if (dp[i][j][k] == INT_MAX)
+                    continue;
+
+                dp[i][j][1] = dp[i][j][k] + prefixSum[j + 1] - prefixSum[i];
             }
         }
         return dp[0][n - 1][1];
