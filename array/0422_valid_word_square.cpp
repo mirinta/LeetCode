dc84678ae1b002fbc56c89e3bc2c3b90b9e108e6
mirinta <mirinta@163.com>
2023-10-25 16:51:43 +0800

    Update 0422_valid_word_square.cpp

--- array/0422_valid_word_square.cpp
+++ array/0422_valid_word_square.cpp
@@ -1,23 +1,26 @@
-#include <algorithm>
 #include <string>
 #include <vector>
 
 /**
- * Given an array of strings "words", return true if it forms a valid word square.
+ * Given an array of strings words, return true if it forms a valid word square.
  *
- * A sequence of strings froms a valid word square if the kth row and column read the same string,
+ * A sequence of strings forms a valid word square if the kth row and column read the same string,
  * where 0 <= k < max(numRows, numColumns).
+ *
+ * ! 1 <= words.length <= 500
+ * ! 1 <= words[i].length <= 500
+ * ! words[i] consists of only lowercase English letters.
  */
 
 class Solution
 {
 public:
-    bool validWordSquare(const std::vector<std::string>& words)
+    bool validWordSquare(std::vector<std::string>& words)
     {
         if (words.empty())
             return false;
 
-        // it must be a square matrix
+        // check whether it is a square matrix
         const size_t m = words.size();
         size_t n = 0;
         for (const auto& word : words) {
@@ -26,11 +29,11 @@ public:
         if (m != n)
             return false;
 
-        // check the given matrix is symmetric
-        for (size_t i = 0; i < m; ++i) {
-            for (size_t j = i + 1; j < n; ++j) {
-                const char c1 = j < words[i].size() ? words[i][j] : '#';
-                const char c2 = i < words[j].size() ? words[j][i] : '#';
+        // check whether it is a symmetric matrix
+        for (int i = 0; i < m; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                const auto& c1 = j < words[i].size() ? words[i][j] : '#';
+                const auto& c2 = i < words[j].size() ? words[j][i] : '#';
                 if (c1 != c2)
                     return false;
             }
