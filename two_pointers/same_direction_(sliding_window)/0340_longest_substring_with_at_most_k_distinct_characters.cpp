af07ff14e239f62d2dedd9a07bb11ad103146470
mirinta <mirinta@163.com>
2023-10-25 18:24:10 +0800

    Update 0340_longest_substring_with_at_most_k_distinct_characters.cpp

--- two_pointers/same_direction_(sliding_window)/0340_longest_substring_with_at_most_k_distinct_characters.cpp
+++ two_pointers/same_direction_(sliding_window)/0340_longest_substring_with_at_most_k_distinct_characters.cpp
@@ -2,8 +2,8 @@
 #include <unordered_map>
 
 /**
- * Given a string s and an integer k, return the length of the longest
- * substring of s that contains at most k distinct characters.
+ * Given a string s and an integer k, return the length of the longest substring of s that contains
+ * at most k distinct characters.
  *
  * ! 1 <= s.length <= 5 * 10^4
  * ! 0 <= k <= 50
@@ -14,22 +14,21 @@ class Solution
 public:
     int lengthOfLongestSubstringKDistinct(std::string s, int k)
     {
-        std::unordered_map<char, int> freq;
-        int left = 0;
-        int right = 0;
+        const int n = s.size();
+        if (n <= k)
+            return n;
+
+        std::unordered_map<char, int> map; // letter to frequency
         int result = 0;
-        // sliding window, elements in range [left, right)
-        while (right < s.size()) {
-            freq[s[right++]]++;
-            while (freq.size() > k) {
-                if (freq[s[left]] == 1) {
-                    freq.erase(s[left]);
-                } else {
-                    freq[s[left]]--;
+        for (int left = 0, right = 0; right < n; ++right) {
+            map[s[right]]++;
+            while (left <= right && map.size() > k) {
+                if (--map[s[left]] == 0) {
+                    map.erase(s[left]);
                 }
                 left++;
             }
-            result = std::max(result, right - left);
+            result = std::max(result, right - left + 1);
         }
         return result;
     }
