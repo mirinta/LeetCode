51a690dc7533265dff97c12bd7caa783e90bf550
mirinta <mirinta@163.com>
2023-10-25 18:20:35 +0800

    Create 0159_longest_substring_with_at_most_two_distinct_characters.cpp

---
+++ two_pointers/same_direction_(sliding_window)/0159_longest_substring_with_at_most_two_distinct_characters.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <unordered_map>
+
+/**
+ * Given a string s, return the length of the longest substring that contains at most two distinct
+ * characters.
+ *
+ * ! 1 <= s.length <= 10^5
+ * ! s consists of English letters.
+ */
+
+class Solution
+{
+public:
+    int lengthOfLongestSubstringTwoDistinct(std::string s)
+    {
+        const int n = s.size();
+        if (n <= 2)
+            return n;
+
+        std::unordered_map<char, int> map; // letter to frequency
+        int result = 0;
+        for (int left = 0, right = 0; right < n; ++right) {
+            map[s[right]]++;
+            while (left <= right && map.size() > 2) {
+                if (--map[s[left]] == 0) {
+                    map.erase(s[left]);
+                }
+                left++;
+            }
+            result = std::max(result, right - left + 1);
+        }
+        return result;
+    }
+};
