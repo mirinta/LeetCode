9c70dbf0e560e8f1fd912ab2e325748f6214edca
mirinta <mirinta@163.com>
2023-10-25 17:39:30 +0800

    Update 0779_kth_symbol_in_grammar.cpp

--- mathematics/0779_kth_symbol_in_grammar.cpp
+++ mathematics/0779_kth_symbol_in_grammar.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 
 /**
  * We build a table of n rows (1-indexed). We start by writing 0 in the 1st row. Now in every
@@ -12,25 +13,32 @@
  * ! 1 <= k <= 2^(n - 1)
  */
 
+#include <cmath>
+
 class Solution
 {
 public:
     int kthGrammar(int n, int k)
     {
+        /**
+         *       k:   (1)     (2)
+         * row i-1:    0       1
+         *            / \     / \
+         *   row i:  0   1   1   0
+         *       k: (1) (2) (3) (4)
+         *
+         * the kth symbol in the ith row comes from the ceil(k/2)th symbol in the i-1th row
+         *
+         * let src = the ceil(k/2)th symbol in the i-1th row
+         * - if k is odd and src = 0, then result = 0
+         * - if k is even and src = 0, then result = 1
+         * - if k is odd and src = 1, then result = 1
+         * - if k is even and src = 1, then result = 0
+         */
         if (n == 1)
             return 0;
 
-        // let (j) denotes the jth number in a row
-        //   i-1th row:   (1)     (2)   ...
-        //                / \     / \   ...
-        //     ith row: (1) (2) (3) (4) ...
-        // consider the values:
-        //   i-1th row:  0   1  ...
-        //              / \ / \ ...
-        //     ith row: 0 1 1 0 ...
-        if (k & 1)
-            return kthGrammar(n - 1, (k + 1) / 2);
-
-        return 1 - kthGrammar(n - 1, k / 2);
+        const int src = kthGrammar(n - 1, std::ceil(k * 0.5));
+        return k % 2 != 0 ? src : 1 - src;
     }
 };
