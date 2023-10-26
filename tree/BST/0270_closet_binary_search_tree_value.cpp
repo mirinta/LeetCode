15150340bab5bb99e5357fae780ecc2f57730117
mirinta <mirinta@163.com>
2023-10-26 18:42:29 +0800

    Update 0270_closet_binary_search_tree_value.cpp

--- tree/BST/0270_closet_binary_search_tree_value.cpp
+++ tree/BST/0270_closet_binary_search_tree_value.cpp
@@ -1,6 +1,3 @@
-#include <algorithm>
-#include <vector>
-
 /**
  * Definition for a binary tree node.
  */
@@ -41,36 +38,5 @@ public:
             root = target < root->val ? root->left : root->right;
         }
         return result;
-    }
-
-private:
-    // inorder traverse all nodes, time O(N), space O(N)
-    int approach1(TreeNode* root, double target)
-    {
-        if (!root)
-            return -1;
-
-        std::vector<int> inorder;
-        traverse(inorder, root);
-        if (target > inorder.back())
-            return inorder.back();
-
-        if (target < inorder.front())
-            return inorder.front();
-
-        auto iter = std::upper_bound(inorder.begin(), inorder.end(), target);
-        const int firstLarger = *iter;
-        const int lastSmaller = *std::prev(iter);
-        return target - lastSmaller <= firstLarger - target ? lastSmaller : firstLarger;
-    }
-
-    void traverse(std::vector<int>& inorder, TreeNode* node)
-    {
-        if (!node)
-            return;
-
-        traverse(inorder, node->left);
-        inorder.push_back(node->val);
-        traverse(inorder, node->right);
     }
 };
