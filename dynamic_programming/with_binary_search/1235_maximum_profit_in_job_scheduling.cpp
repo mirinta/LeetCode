65a8c6a4bb357337c70e8aff96de7ad5a8e55183
mirinta <mirinta@163.com>
2024-01-06 14:10:33 +0800

    1235

--- dynamic_programming/with_binary_search/1235_maximum_profit_in_job_scheduling.cpp
+++ dynamic_programming/with_binary_search/1235_maximum_profit_in_job_scheduling.cpp
@@ -1,5 +1,5 @@
 #include <algorithm>
-#include <unordered_map>
+#include <array>
 #include <vector>
 
 /**
@@ -23,45 +23,29 @@ public:
     int jobScheduling(std::vector<int>& startTime, std::vector<int>& endTime,
                       std::vector<int>& profit)
     {
-        const int n = startTime.size();
-        std::vector<std::vector<int>> jobs(n, std::vector<int>(3, 0)); // <start, end, profit>
+        const int n = profit.size();
+        std::vector<std::array<int, 3>> jobs(n); // <start, end, profit>
         for (int i = 0; i < n; ++i) {
             jobs[i][0] = startTime[i];
             jobs[i][1] = endTime[i];
             jobs[i][2] = profit[i];
         }
-        // sort by ending times
+        // sort jobs by ending time
         std::sort(jobs.begin(), jobs.end(),
-                  [](const auto& v1, const auto& v2) { return v1[1] < v2[1]; });
-        // dp[i] = max profit at the end of i_end, i represents the ith job
-        // #NOTE# jobs are sorted by ending times in ascending order
-        // X---j_end
-        //   X------X
-        //        i_start----i_end
-        int result = 0;
-        std::unordered_map<int, int> dp;
+                  [](const auto& job1, const auto& job2) { return job1[1] < job2[1]; });
+        // dp[i] = max profit of scheduling jobs[0:i]
+        std::vector<int> dp(n, INT_MIN);
+        int result = INT_MIN;
         for (int i = 0; i < n; ++i) {
-            // choice 1: we don't choose jobs[i], dp[i] = dp[i-1]
-            // choice 2: choose jobs[i]
-            // - find the last j such that j_end <= i_start
-            // - if j is found, dp[i] = max(dp[i-1], dp[j] + profit[i])
-            // - if j is not found, dp[i] = max(dp[i-1], profit[i])
-            int lo = 0;
-            int hi = i;
-            while (lo <= hi) {
-                const int mid = lo + (hi - lo) / 2;
-                if (jobs[mid][1] <= jobs[i][0]) {
-                    lo = mid + 1;
-                } else {
-                    hi = mid - 1;
-                }
+            const auto& [start, end, gain] = jobs[i];
+            dp[i] = i > 0 ? std::max(dp[i - 1], gain) : gain;
+            // find the largest j such that jobs[j].end <= jobs[i].start
+            auto iter = std::upper_bound(jobs.begin(), jobs.end(), start,
+                                         [](int time, const auto& job) { return job[1] > time; });
+            if (iter != jobs.begin()) {
+                dp[i] = std::max(dp[i], dp[std::prev(iter) - jobs.begin()] + gain);
             }
-            if (hi < 0) {
-                dp[i] = std::max(dp[i - 1], jobs[i][2]);
-            } else {
-                dp[i] = std::max(dp[i - 1], dp[hi] + jobs[i][2]);
-            }
-            result = std::max(dp[i], result);
+            result = std::max(result, dp[i]);
         }
         return result;
     }
