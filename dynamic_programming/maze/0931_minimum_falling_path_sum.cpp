bc1ccc6e4d45acce9b11dbce5e6d86558d14beff
mirinta <mirinta@163.com>
2024-01-19 10:51:55 +0800

    931

--- dynamic_programming/maze/0931_minimum_falling_path_sum.cpp
+++ dynamic_programming/maze/0931_minimum_falling_path_sum.cpp
@@ -1,53 +1,64 @@
-#include <climits>
+#include <algorithm>
 #include <vector>
 
 /**
- * Given an "n x n" array of integers "matrix", return the minimum sum of any falling path through
- * "matrix".
+ * Given an n x n array of integers matrix, return the minimum sum of any falling path through
+ * matrix.
  *
  * A falling path starts at any element in the first row and chooses the element in the next row
- * that is either directly below or diagonally left/right. Specially, the next element from position
- * (row, col) will be (row + 1, col - 1), (row + 1, col), and (row + 1, col + 1).
+ * that is either directly below or diagonally left/right. Specifically, the next element from
+ * position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
  *
- * Example:
- * [ 2   #1#   3 ]  [ 2   #1#   3 ]
- * [ 6   #5#   4 ]  [ 6    5   #4#]
- * [#7#   8    9 ]  [ 7   #8#   9 ]
- *
- * There are two falling paths with a minimum sum (13): 1->5->7 and 1->4->8.
+ * ! n == matrix.length == matrix[i].length
+ * ! 1 <= n <= 100
+ * ! -100 <= matrix[i][j] <= 100
  */
 
 class Solution
 {
 public:
-    int minFallingPathSum(std::vector<std::vector<int>>& matrix)
+    int minFallingPathSum(std::vector<std::vector<int>>& matrix) { return approach2(matrix); }
+
+private:
+    // DP with space optimization, TC = O(N^2), SC = O(N)
+    int approach2(const std::vector<std::vector<int>>& matrix)
     {
-        const auto n = matrix.size();
-        // dp[i][j] = the minimum sum of falling path from position (0, ...) to position (i, j)
-        // There are three source positions that can fall to (i, j),
-        // so dp[i][j] = val[i][j] + min(dp[i - 1][j], dp[i - 1][j + 1], dp[i - 1][j - 1])
-        // The above equation is only dependent on the previous row,
-        // so it's not necessary to make a new DP matrix, we can directly solve the problem
-        // in-place. The loop begins from the second row, because the first row needs to remain the
-        // same.
+        const int n = matrix.size();
+        auto dp = matrix[0];
         for (int i = 1; i < n; ++i) {
+            auto prev = dp;
+            std::fill(dp.begin(), dp.end(), INT_MAX);
             for (int j = 0; j < n; ++j) {
-                auto val = matrix[i - 1][j];
+                dp[j] = std::min(dp[j], matrix[i][j] + prev[j]);
                 if (j - 1 >= 0) {
-                    val = std::min(val, matrix[i - 1][j - 1]);
+                    dp[j] = std::min(dp[j], matrix[i][j] + prev[j - 1]);
                 }
                 if (j + 1 < n) {
-                    val = std::min(val, matrix[i - 1][j + 1]);
+                    dp[j] = std::min(dp[j], matrix[i][j] + prev[j + 1]);
                 }
-                matrix[i][j] += val;
             }
         }
-        int result = INT_MAX;
-        for (const auto& v : matrix[n - 1]) {
-            if (v < result) {
-                result = v;
+        return *std::min_element(dp.begin(), dp.end());
+    }
+
+    // DP, TC = O(N^2), SC = O(N^2)
+    int approach1(const std::vector<std::vector<int>>& matrix)
+    {
+        // dp[i][j] = min sum of falling path from first row to cell(i,j)
+        const int n = matrix.size();
+        std::vector<std::vector<int>> dp(n, std::vector<int>(n, INT_MAX));
+        dp[0] = matrix[0];
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                dp[i][j] = std::min(dp[i][j], matrix[i][j] + dp[i - 1][j]);
+                if (j - 1 >= 0) {
+                    dp[i][j] = std::min(dp[i][j], matrix[i][j] + dp[i - 1][j - 1]);
+                }
+                if (j + 1 < n) {
+                    dp[i][j] = std::min(dp[i][j], matrix[i][j] + dp[i - 1][j + 1]);
+                }
             }
         }
-        return result;
+        return *std::min_element(dp[n - 1].begin(), dp[n - 1].end());
     }
 };
