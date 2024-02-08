f6e7fb6430f65513242efdd1da36086b97325fc8
mirinta <mirinta@163.com>
2024-02-08 13:07:15 +0800

    279

--- dynamic_programming/knapsack/0279_perfect_squares.cpp
+++ dynamic_programming/knapsack/0279_perfect_squares.cpp
@@ -16,18 +16,9 @@ public:
     int numSquares(int n)
     {
         // dp[i] = min num of perfect square numbers that sum to i
-        // base case:
-        // - dp[0] = 0
-        // - dp[X] = 1, X is a perfect square number <= n, and it is the optimal answer
-        std::vector<int> dp(1 + n, INT_MAX);
+        std::vector<int> dp(n + 1, INT_MAX);
         dp[0] = 0;
-        for (int i = 1; i * i <= n; ++i) {
-            dp[i * i] = 1;
-        }
         for (int i = 1; i <= n; ++i) {
-            if (dp[i] == 1)
-                continue;
-
             for (int j = 1; j * j <= i; ++j) {
                 dp[i] = std::min(dp[i], 1 + dp[i - j * j]);
             }
