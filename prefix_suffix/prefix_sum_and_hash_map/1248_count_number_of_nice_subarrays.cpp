9a64ca74a7987980e3780df961106c5841d9d1fc
mirinta <mirinta@163.com>
2024-06-22 11:03:12 +0800

    solutions

--- prefix_suffix/prefix_sum_and_hash_map/1248_count_number_of_nice_subarrays.cpp
+++ prefix_suffix/prefix_sum_and_hash_map/1248_count_number_of_nice_subarrays.cpp
@@ -21,16 +21,15 @@ private:
     // sliding window, TC = O(N), SC = O(1)
     int approach2(const std::vector<int>& nums, int k)
     {
-        return solve(nums, k) - solve(nums, k - 1);
+        return helper(nums, k) - helper(nums, k - 1);
     }
 
-    // num of subarrays with num of odd numbers <= k
-    int solve(const std::vector<int>& nums, int k)
+    // num of subarrays that each subarray contains at most k odd numbers
+    int helper(const std::vector<int>& nums, int k)
     {
         const int n = nums.size();
-        int count = 0;
         int result = 0;
-        for (int left = 0, right = 0; right < n; ++right) {
+        for (int left = 0, right = 0, count = 0; right < n; ++right) {
             count += nums[right] % 2;
             while (count > k) {
                 count -= nums[left] % 2;
@@ -44,7 +43,7 @@ private:
     // prefix sum + hash map, TC = O(N), SC = O(N)
     int approach1(const std::vector<int>& nums, int k)
     {
-        // let count[i] = num of odd numbers of nums[0:i]
+        // let count[i] = num of odd numbers in nums[0:i]
         // if nums[j+1:i] is a valid subarray,
         // then count[i] - count[j] = k
         const int n = nums.size();
@@ -52,11 +51,13 @@ private:
         // count[j] = count[i] - k = 0
         std::unordered_map<int, int> map;
         map[0] = 1;
-        int count = 0;
         int result = 0;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0, count = 0; i < n; ++i) {
             count += nums[i] % 2;
-            result += map[count - k];
+            const int target = count - k;
+            if (target >= 0 && map.count(target)) {
+                result += map[target];
+            }
             map[count]++;
         }
         return result;
