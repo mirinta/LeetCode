4a5e99e85216174135961bb19a9eb3b13fcaf8d4
mirinta <mirinta@163.com>
2024-07-11 00:56:04 +0800

    1598

---
+++ string/1598_crawler_log_folder.cpp
@@ -0,0 +1,51 @@
+#include <string>
+#include <vector>
+
+/**
+ * The Leetcode file system keeps a log each time some user performs a change folder operation.
+ *
+ * The operations are described below:
+ *
+ * - "../" : Move to the parent folder of the current folder. (If you are already in the main
+ * folder, remain in the same folder).
+ *
+ * - "./" : Remain in the same folder.
+ *
+ * - "x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+ *
+ * You are given a list of strings logs where logs[i] is the operation performed by the user at the
+ * ith step.
+ *
+ * The file system starts in the main folder, then the operations in logs are performed.
+ *
+ * Return the minimum number of operations needed to go back to the main folder after the change
+ * folder operations.
+ *
+ * ! 1 <= logs.length <= 10^3
+ * ! 2 <= logs[i].length <= 10
+ * ! logs[i] contains lowercase English letters, digits, '.', and '/'.
+ * ! logs[i] follows the format described in the statement.
+ * ! Folder names consist of lowercase English letters and digits.
+ */
+
+class Solution
+{
+public:
+    int minOperations(std::vector<std::string>& logs)
+    {
+        int result = 0;
+        for (const auto& s : logs) {
+            if (s == "./")
+                continue;
+
+            if (s != "../") {
+                result++;
+                continue;
+            }
+            if (result > 0) {
+                result--;
+            }
+        }
+        return result;
+    }
+};
