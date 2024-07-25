c10d3b7fc2f5a1c034720ecd1f6e10a6a145d4e7
mirinta <mirinta@163.com>
2024-07-25 10:09:40 +0800

    Update 0912_sort_an_array.cpp

--- array/0912_sort_an_array.cpp
+++ array/0912_sort_an_array.cpp
@@ -8,129 +8,40 @@
  * with the smallest space complexity possible.
  */
 
-class Solution
-{
-    // approach 1: quick sort
+class Solution {
 public:
-    std::vector<int> sortArray(const std::vector<int>& nums)
-    {
-        if (nums.empty())
-            return {};
-
-        std::vector<int> result(nums);
-        std::random_device rd;
-        std::mt19937 g(rd());
-        std::shuffle(result.begin(), result.end(), g);
-        quickSort3Way(result, 0, result.size() - 1);
-        return result;
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums);
+        return nums;
     }
 
 private:
-    void quickSort(std::vector<int>& nums, int low, int high)
-    {
-        if (low >= high)
-            return;
-
-        int pos = partition(nums, low, high);
-        quickSort(nums, low, pos - 1);
-        quickSort(nums, pos + 1, high);
+    void mergeSort(std::vector<int>& nums) {
+        const int n = nums.size();
+        std::vector<int> aux(n);
+        mergeSort(nums, aux, 0, n - 1);
     }
 
-    int partition(std::vector<int>& nums, int low, int high)
-    {
-        int pivot = nums[low];
-        // split [i, j] to [i, x - 1] and [x + 1, high] such that
-        // - elements in [i, x - 1] <= pivot, and
-        // - elements in [x + 1, high] > pivot;
-        int i = low + 1;
-        int j = high;
-        while (true) {
-            while (i <= high && nums[i] <= pivot) {
-                i++;
-            }
-            while (j >= low && nums[j] > pivot) {
-                j--;
-            }
-            if (i >= j)
-                break;
+    void mergeSort(std::vector<int>& nums, std::vector<int>& aux, int lo, int hi) {
+        if (lo >= hi)
+            return;
 
-            std::swap(nums[i], nums[j]);
+        const int mid = lo + (hi - lo) / 2;
+        mergeSort(nums, aux, lo, mid);
+        mergeSort(nums, aux, mid + 1, hi);
+        merge(nums, aux, lo, mid, hi);
+    }
+
+    void merge(std::vector<int>& nums, std::vector<int>& aux, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; ++k) {
+            aux[k] = nums[k];
         }
-        std::swap(nums[low], nums[j]);
-        return j;
-    }
-
-    void quickSort3Way(std::vector<int>& nums, int low, int high)
-    {
-        if (low >= high)
-            return;
-
-        // [low, lt - 1] < pivot
-        // [lt, gt] == pivot
-        // [gt + 1, high] > pivot
-        int pivot = nums[low];
-        int lt = low;
-        int gt = high;
-        int i = low + 1;
-        while (i <= gt) {
-            if (nums[i] < pivot) {
-                std::swap(nums[i++], nums[lt++]);
-            } else if (nums[i] > pivot) {
-                std::swap(nums[i], nums[gt--]);
+        for (int i = lo, j = mid + 1, k = lo; k <= hi; ++k) {
+            if (i > mid || (j <= hi && aux[j] < aux[i])) {
+                nums[k] = aux[j++];
             } else {
-                i++;
+                nums[k] = aux[i++];
             }
         }
-        quickSort3Way(nums, low, lt - 1);
-        quickSort3Way(nums, gt + 1, high);
     }
-    // ------------------------------
-    // approach 2: merge sort
-    // public:
-    //     std::vector<int> sortArray(const std::vector<int>& nums)
-    //     {
-    //         if (nums.empty())
-    //             return {};
-
-    //         std::vector<int> result(nums);
-    //         std::vector<int> auxiliary(nums.size(), 0);
-    //         mergeSort(result, auxiliary, 0, result.size() - 1);
-    //         return result;
-    //     }
-
-    // private:
-    //     void mergeSort(std::vector<int>& result, std::vector<int>& auxiliary, int low, int high)
-    //     {
-    //         if (low >= high)
-    //             return;
-
-    //         const int mid = low + (high - low) / 2;
-    //         mergeSort(result, auxiliary, low, mid);
-    //         mergeSort(result, auxiliary, mid + 1, high);
-    //         merge(result, auxiliary, low, mid, high);
-    //     }
-
-    //     // merge two sorted sub-arrays, [low, mid] and [mid + 1, high]
-    //     void merge(std::vector<int>& result, std::vector<int>& auxiliary, int low, int mid, int
-    //     high)
-    //     {
-    //         for (int i = low; i <= high; ++i) {
-    //             auxiliary[i] = result[i];
-    //         }
-    //         for (int i = low, j = mid + 1, k = low; k <= high; ++k) {
-    //             if (i > mid) {
-    //                 result[k] = auxiliary[j++];
-    //                 continue;
-    //             }
-    //             if (j > high) {
-    //                 result[k] = auxiliary[i++];
-    //                 continue;
-    //             }
-    //             if (auxiliary[i] > auxiliary[j]) {
-    //                 result[k] = auxiliary[j++];
-    //             } else {
-    //                 result[k] = auxiliary[i++];
-    //             }
-    //         }
-    //     }
 };
