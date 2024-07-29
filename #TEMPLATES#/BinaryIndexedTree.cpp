c90f188c805aed7b2bbfcdbea0d9a77c43a5d4e4
mirinta <mirinta@163.com>
2024-07-29 14:40:13 +0800

    Update BinaryIndexedTree.cpp

--- #TEMPLATES#/BinaryIndexedTree.cpp
+++ #TEMPLATES#/BinaryIndexedTree.cpp
@@ -2,57 +2,48 @@
 #include <vector>
 
 /**
- * BinaryIndexedTree(int n): n is the num of elements, i.e., n = nums.size()
+ * BinaryIndexedTree(LLONG n): n is the num of elements, i.e., n = nums.size()
  *
- * add(int i, long long delta): make nums[i] += delta, i is 1-indexed
+ * add(LLONG i, LLONG delta): increase nums[i] by delta, i is 1-indexed
  *
- * query(int left, int right): query the sum of nums[left:right], left and right are 1-indexed
+ * query(LLONG left, LLONG right): query the sum of nums[left:right], left and right are 1-indexed
  *
- * presum(int i): return the sum of nums[1:i], i is 1-indexed
+ * presum(LLONG i): return the sum of nums[1:i], i is 1-indexed
  *
- * std::vector<long long> tree: a 1-indexed array
+ * std::vector<LLONG> tree: a 1-indexed array
  */
 class BinaryIndexedTree
 {
+    using LLONG = long long;
+
 public:
-    explicit BinaryIndexedTree(int n) : tree(n + 1, 0) {}
+    explicit BinaryIndexedTree(LLONG n) : tree(n + 1) {}
 
-    void add(int i, long long delta)
+    void add(LLONG i, LLONG delta)
     {
-        if (!validate(i))
-            return;
-
-        while (i < tree.size()) {
+        for (; i < tree.size(); i += lowbit(i)) {
             tree[i] += delta;
-            i += lowbit(i);
         }
     }
 
-    long long query(int left, int right)
-    {
-        if (left > right || !validate(left) || !validate(right))
-            return 0;
+    void reset() { std::fill(tree.begin(), tree.end(), 0); }
 
-        return presum(right) - presum(left - 1);
-    }
+    LLONG query(LLONG left, LLONG right) { return presum(right) - presum(left - 1); }
 
 private:
-    int lowbit(int i) { return i & (-i); }
+    LLONG lowbit(LLONG i) { return i & (-i); }
 
-    bool validate(int i) const { return i >= 1 && i < tree.size(); }
-
-    long long presum(int i)
+    LLONG presum(LLONG i)
     {
-        long long sum = 0;
-        while (i > 0) {
+        LLONG sum = 0;
+        for (; i > 0; i -= lowbit(i)) {
             sum += tree[i];
-            i -= lowbit(i);
         }
         return sum;
     }
 
 private:
-    std::vector<long long> tree;
+    std::vector<LLONG> tree;
 };
 
 int main()
