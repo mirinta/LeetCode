ec1327c30042d0a8aeb8ce7dd41c47d7bdbbd598
mirinta <mirinta@163.com>
2024-08-14 23:40:55 +0800

    719

--- binary_search/0719_find_kth_smallest_pair_distance.cpp
+++ binary_search/0719_find_kth_smallest_pair_distance.cpp
@@ -23,7 +23,7 @@ public:
         int hi = nums.back() - nums.front();
         while (lo < hi) {
             const int mid = lo + (hi - lo) / 2;
-            if (countSmallerOrEqual(mid, nums) >= k) {
+            if (isValid(mid, nums, k)) {
                 hi = mid;
             } else {
                 lo = mid + 1;
@@ -33,16 +33,25 @@ public:
     }
 
 private:
-    int countSmallerOrEqual(int diff, const std::vector<int>& nums)
+    // count the num of pairs that the absolute difference of each pair <= diff
+    // and check if the num of pairs >= k
+    // nums is sorted in non-decreasing order
+    bool isValid(int diff, const std::vector<int>& nums, int k)
     {
         const int n = nums.size();
         int count = 0;
-        for (int i = 0, j = 0; j < n; ++j) {
-            while (nums[j] - nums[i] > diff) {
-                i++;
+        for (int left = 0, right = 0; right < n; ++right) {
+            while (nums[right] - nums[left] > diff) {
+                left++;
             }
-            count += j - i;
+            // nums[right] and nums[right-1] is valid
+            // nums[right] and nums[right-2] is valid
+            // ...
+            // nums[right] and nums[left] is valid
+            count += right - left;
+            if (count >= k)
+                return true;
         }
-        return count;
+        return false;
     }
 };
