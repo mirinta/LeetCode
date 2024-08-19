772443720eb662ab07baf17851a8a09816af37ef
mirinta <mirinta@163.com>
2024-08-20 02:23:48 +0800

    650

--- dynamic_programming/keyboard/0650_2_keys_keyboard.cpp
+++ dynamic_programming/keyboard/0650_2_keys_keyboard.cpp
@@ -21,22 +21,18 @@ public:
     int minSteps(int n)
     {
         // dp[i] = min operations to get exactly i 'A's
-        // if we only copy one 'A', then we need at most n operations to get n 'A's
-        // - it means dp[i] <= i
-        // - corner case is dp[1] = 0
-        // X X X X X X copy paste paste ... paste
-        // |<--#A=j->|      |<-----x times----->|
-        // |<---------------#A=i--------------->|
-        // i = (x + 1) * j, x = i/j + 1
-        // #operations = dp[j] + x + 1 = dp[j] + i/j
-        std::vector<int> dp(1 + n, 0);
-        dp[1] = 0;
+        // A A A A ... A copy paste ... paste
+        // |<---#A=j-->|
+        // |<-------------#A=i------------->|
+        // let x = num of paste operations
+        // i = j + x * j, x = i / j - 1 >= 1, then i / j >= 2
+        // #operations = dp[j] + x + 1 = dp[j] + i / j
+        std::vector<int> dp(n + 1, 0);
         for (int i = 2; i <= n; ++i) {
             dp[i] = i;
-            for (int j = i - 2; j >= 2; --j) {
+            for (int j = 1; j <= i / 2; ++j) {
                 if (i % j == 0) {
                     dp[i] = std::min(dp[i], dp[j] + i / j);
-                    break;
                 }
             }
         }
