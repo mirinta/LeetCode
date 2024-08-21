8a4fc0f7b4af47cfd455e76c629587b90992fc16
mirinta <mirinta@163.com>
2024-08-21 11:01:09 +0800

    Update 0664_strange_printer.cpp

--- dynamic_programming/interval_II/0664_strange_printer.cpp
+++ dynamic_programming/interval_II/0664_strange_printer.cpp
@@ -20,29 +20,51 @@ class Solution
 public:
     int strangePrinter(std::string s)
     {
-        // dp[i][j] = min num of turns to print s[i:j]
+        const auto simplified = process(s);
+        const int n = simplified.size();
+        std::vector<std::vector<int>> memo(n, std::vector<int>(n, -1));
+        return dfs(memo, 0, n - 1, simplified);
+    }
+
+private:
+    // remove consecutive duplicates, e.g. aaabbb -> ab
+    std::string process(const std::string& s)
+    {
         const int n = s.size();
-        std::vector<std::vector<int>> dp(n, std::vector<int>(n, INT_MAX / 2));
-        for (int i = 0; i < n; ++i) {
-            dp[i][i] = 1;
-        }
-        for (int length = 2; length <= n; ++length) {
-            for (int i = 0; i + length - 1 < n; ++i) {
-                const int j = i + length - 1;
-                // case 1: print single s[i]
-                dp[i][j] = std::min(dp[i][j], 1 + dp[i + 1][j]);
-                // case 2: s[i] = s[j], then s[j] can be printed when processing s[i:j-1]
-                if (s[i] == s[j]) {
-                    dp[i][j] = std::min(dp[i][j], dp[i][j - 1]);
-                }
-                // case 3: s[i] = s[k], then s[k] can be printed when processing s[i:k-1]
-                for (int k = i + 1; k < j; ++k) {
-                    if (s[i] == s[k]) {
-                        dp[i][j] = std::min(dp[i][j], dp[i][k - 1] + dp[k + 1][j]);
-                    }
-                }
+        std::string result;
+        int i = 0;
+        while (i < n) {
+            if (result.empty() || s[i] != result.back()) {
+                result.push_back(s[i]);
             }
+            i++;
         }
-        return dp[0][n - 1];
+        return result;
+    }
+
+    // min num of operations to print s[lo:hi]
+    int dfs(std::vector<std::vector<int>>& memo, int lo, int hi, const std::string& s)
+    {
+        if (lo > hi)
+            return 0;
+
+        if (memo[lo][hi] != -1)
+            return memo[lo][hi];
+
+        // case 1: print one s[lo]
+        // lo X X X ... X X hi
+        //    |<-next round->|
+        int result = 1 + dfs(memo, lo + 1, hi, s);
+        for (int k = lo + 1; k <= hi; ++k) {
+            if (s[lo] != s[k])
+                continue;
+
+            // case 2: print a sequence of s[lo]
+            // if s[lo] = s[k], then let s[lo:k] = s[lo]
+            // lo X X X ... k-1 k k+1 ... X X X hi
+            // |<-next round->|   |<-next round->|
+            result = std::min(result, dfs(memo, lo, k - 1, s) + dfs(memo, k + 1, hi, s));
+        }
+        return memo[lo][hi] = result;
     }
 };
