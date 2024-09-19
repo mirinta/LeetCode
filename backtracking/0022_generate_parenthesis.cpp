ded570a587504d82ec21aa64f3f8efca7c3951e0
mirinta <mirinta@163.com>
2024-09-19 20:32:34 +0800

    Update 0022_generate_parenthesis.cpp

--- backtracking/0022_generate_parenthesis.cpp
+++ backtracking/0022_generate_parenthesis.cpp
@@ -13,30 +13,28 @@ class Solution
 public:
     std::vector<std::string> generateParenthesis(int n)
     {
-        std::string path;
-        backtrack(path, 0, 0, n);
+        std::vector<std::string> result;
+        std::string current;
+        backtrack(result, current, 0, 0, n);
         return result;
     }
 
 private:
-    std::vector<std::string> result;
-
-    // enumerate the ith character of a well-formed parentheses
-    // only two choices: ( or )
-    // num of left parenthesis >= num of right parenthesis
-    void backtrack(std::string& path, int leftParenthesis, int rightParenthesis, int n)
+    void backtrack(std::vector<std::string>& result, std::string& current, int left, int right,
+                   int n)
     {
-        if (leftParenthesis < rightParenthesis || leftParenthesis > n || rightParenthesis > n)
+        if (left < right || left > n || right > n)
             return;
 
-        if (leftParenthesis == n && rightParenthesis == n) {
-            result.push_back(path);
+        if (left == right && left == n) {
+            result.push_back(current);
+            return;
         }
-        path.push_back('(');
-        backtrack(path, leftParenthesis + 1, rightParenthesis, n);
-        path.pop_back();
-        path.push_back(')');
-        backtrack(path, leftParenthesis, rightParenthesis + 1, n);
-        path.pop_back();
+        current.push_back('(');
+        backtrack(result, current, left + 1, right, n);
+        current.pop_back();
+        current.push_back(')');
+        backtrack(result, current, left, right + 1, n);
+        current.pop_back();
     }
 };
