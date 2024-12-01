fe198ca530bd3d7507985ffe8d49f7a6228231ca
mirinta <mirinta@163.com>
2024-12-01 22:11:15 +0800

    1346

--- array/1346_check_if_n_and_its_double_exist.cpp
+++ array/1346_check_if_n_and_its_double_exist.cpp
@@ -16,26 +16,15 @@ class Solution
 public:
     bool checkIfExist(std::vector<int>& arr)
     {
-        if (arr.size() < 2)
-            return false;
+        std::unordered_set<int> set;
+        for (const auto& val : arr) {
+            if (set.count(2 * val))
+                return true;
 
-        // approach 1: hash set
-        // std::unordered_set<int> set;
-        // for (const auto& i : arr) {
-        //     if (set.count(2 * i))
-        //         return true;
+            if (val % 2 == 0 && set.count(val / 2))
+                return true;
 
-        //     if (i % 2 == 0 && set.count(i / 2))
-        //         return true;
-
-        //     set.insert(i);
-        // }
-        // approach 2: traverse
-        for (size_t i = 0; i < arr.size() - 1; ++i) {
-            for (size_t j = 0; j < arr.size(); ++j) {
-                if (i != j && arr[i] == arr[j] * 2)
-                    return true;
-            }
+            set.insert(val);
         }
         return false;
     }
